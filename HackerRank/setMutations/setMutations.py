5fa00c3709e0f60c31da445017a7c5c896c06d65
aurimas13 <aurimas.nausedas@gmail.com>
2022-05-06 18:21:43 +0300

    Updated solution

--- HackerRank/setMutations/setMutations.py
+++ HackerRank/setMutations/setMutations.py
@@ -4,12 +4,20 @@ elements_in_N = int(input())
 for i in range(elements_in_N):
     operation_length_other_sets = input().split()
     N = set(map(int, input().split()))
-    if operation_length_other_sets[0] == "intersection_update ":
+    if operation_length_other_sets[0] == "intersection_update":
         A.intersection_update(N)
+        A = A
+        # print(A)
     elif operation_length_other_sets[0] == "update":
         A.update(N)
+        A = A
+        # print(A)
     elif operation_length_other_sets[0] == "symmetric_difference_update":
         A.symmetric_difference_update(N)
+        A = A
+        # print(A)
     elif operation_length_other_sets[0] == "difference_update":
-
+        A.difference_update(N)
+        A = A
+        # print(A)
 print(sum(list(A)))
