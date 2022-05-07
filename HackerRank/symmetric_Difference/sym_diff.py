4429a3f690a1059a2d015d49ed86833f5db27a38
aurimas13 <aurimas.nausedas@gmail.com>
2022-05-07 09:27:47 +0300

    Added solution & description

--- HackerRank/symmetric_Difference/sym_diff.py
+++ HackerRank/symmetric_Difference/sym_diff.py
@@ -3,4 +3,3 @@ english_std_list = set(map(int,input().strip().split()))
 french_std_count = int(input())
 french_std_list = set(map(int,input().strip().split()))
 print(len(english_std_list.symmetric_difference(french_std_list)))
-
