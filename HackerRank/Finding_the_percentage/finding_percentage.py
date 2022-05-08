6e3a65420713da819e50af4eab8e2743ad609042
aurimas13 <aurimas.nausedas@gmail.com>
2022-05-08 09:59:47 +0300

    Update solution

--- HackerRank/Finding_the_percentage/finding_percentage.py
+++ HackerRank/Finding_the_percentage/finding_percentage.py
@@ -9,16 +9,9 @@ if __name__ == '__main__':
         student_marks[name] = scores
     query_name = input()
 
-# Extract the values into a list: query_scores
 query_scores = student_marks[query_name]
-
-# Sum the scores in the list: total_scores
 total_scores = sum(query_scores)
-
-# Convert the floats to decimals and average the scores: avg
 avg = Decimal(total_scores / 3)
-
-# Print the mean of the scores, correct to two decimals
 print(round(avg, 2))
 
 
