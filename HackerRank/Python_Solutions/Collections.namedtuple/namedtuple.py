56b367015949faf94c3fd8eb0a73f487e76193dd
aurimas13 <aurimas.nausedas@gmail.com>
2022-05-09 18:25:14 +0300

    Added solution & description

--- HackerRank/Python_Solutions/Collections.namedtuple/namedtuple.py
+++ HackerRank/Python_Solutions/Collections.namedtuple/namedtuple.py
@@ -1,10 +1,10 @@
 from collections import namedtuple
 
-N = int(input())
+n = int(input())
 a = input()
 total = 0
 Student = namedtuple('Student', a)
-for _ in range(N):
+for _ in range(n):
     student = Student(*input().split())
     total += int(student.MARKS)
 print('{:.2f}'.format(total/n))
