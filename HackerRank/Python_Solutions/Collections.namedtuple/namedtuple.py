75ef846f0ca7c6296310d106399eab62040e6583
aurimas13 <aurimas.nausedas@gmail.com>
2022-05-09 17:02:19 +0300

    Update

--- HackerRank/Python_Solutions/Collections.namedtuple/namedtuple.py
+++ HackerRank/Python_Solutions/Collections.namedtuple/namedtuple.py
@@ -1,2 +1,10 @@
+from collections import namedtuple
 N = int(input())
-names = *list(map(str, input()))
+names = list(map(str, in
+
+             put().split()))
+print(names)
+for i in range(N):
+    id = namedtuple('id', input().split())
+    print(id)
+
