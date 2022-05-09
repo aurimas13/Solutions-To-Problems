9ddcd183c67c5f27cdf5aec5542963172c87d220
aurimas13 <aurimas.nausedas@gmail.com>
2022-05-09 18:19:55 +0300

    Added solution & description

--- HackerRank/Python_Solutions/Collections.namedtuple/namedtuple.py
+++ HackerRank/Python_Solutions/Collections.namedtuple/namedtuple.py
@@ -1,10 +1,36 @@
 from collections import namedtuple
+
 N = int(input())
-names = list(map(str, in
+a = input()
+total = 0
+Student = namedtuple('Student', a)
+for _ in range(N):
+    student = Student(*input().split())
+    total += int(student.MARKS)
+print('{:.2f}'.format(total/n))
 
-             put().split()))
-print(names)
-for i in range(N):
-    id = namedtuple('id', input().split())
-    print(id)
+# or
 
+# N = int(input())
+# Fields = input().split()
+#
+# sum = 0
+# for i in range(N):
+#     Students = namedtuple('Student', Fields)
+#     MARKS, ID, NAME,CLASS = input().split()
+#     Student = Students(MARKS,ID,NAME,CLASS)
+#     sum += int(Student.MARKS)
+# print('{:.2f}'.format(sum/N))
+#
+# # or
+#
+# N = int(input())
+# Names = ' '.join(input.split())
+# Student = namedtuple('Student', Names)
+#
+# for i in range(N):
+#     row = input().split()
+#     student = Student(*row)
+#     sum += int(student.MARKS)
+#
+# print(sum / n)
