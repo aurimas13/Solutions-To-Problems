031902dc30302a6755079d76c298bb53e2caf7f1
aurimas13 <aurimas.nausedas@gmail.com>
2022-05-11 15:03:21 +0300

    Added solution & description

---
+++ HackerRank/Python_Solutions/Any_or_All/any_or_all.py
@@ -0,0 +1,28 @@
+# def isPositive(i):
+#     if i > 0:
+#         return True
+#     return False
+#
+#
+# def isPalindrome(i):
+#     if int(str(i)[::-1]) is i:
+#         return True
+#     return False
+#
+#
+# N = int(input())
+# list_N = map(int, input().split())
+# list_N = sorted(list_N)
+#
+# if all([isPositive(i) for i in list_N]):
+#     if any([isPalindrome(i) for i in list_N]):
+#         print("True")
+#     else:
+#         print("False")
+# else:
+#     print("False")
+
+# or
+
+N, list_N = int(input()),list(map(str, input().split()))
+print(all([int(i)>0 for i in list_N]) and any([j == j[::-1] for j in list_N]))
