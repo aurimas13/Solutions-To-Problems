ef72ebd733c64a250afd11d7e58d26a03373594a
aurimas13 <aurimas.nausedas@gmail.com>
2022-05-12 17:12:52 +0300

    Updated solution

--- HackerRank/Python_Solutions/Any_or_All/any_or_all.py
+++ HackerRank/Python_Solutions/Any_or_All/any_or_all.py
@@ -1,38 +1,26 @@
-import re
+def isPositive(i):
+    if i > 0:
+        return True
+    return False
 
-values = re.findall(r'(?<=[qwrtypsdfghjklzxcvbnm])([aeiou]{2,})(?=[qwrtypsdfghjklzxcvbnm])', input().strip(),re.I)
 
-if values:
-    for i in values:
-        print(i)
+def isPalindrome(i):
+    if int(str(i)[::-1]) is i:
+        return True
+    return False
+
+
+N = int(input())
+list_N = map(int, input().split())
+list_N = sorted(list_N)
+
+if all([isPositive(i) for i in list_N]):
+    if any([isPalindrome(i) for i in list_N]):
+        print("True")
+    else:
+        print("False")
 else:
-    print(-1)
-
-# or
-
-# def isPositive(i):
-#     if i > 0:
-#         return True
-#     return False
-#
-#
-# def isPalindrome(i):
-#     if int(str(i)[::-1]) is i:
-#         return True
-#     return False
-#
-#
-# N = int(input())
-# list_N = map(int, input().split())
-# list_N = sorted(list_N)
-#
-# if all([isPositive(i) for i in list_N]):
-#     if any([isPalindrome(i) for i in list_N]):
-#         print("True")
-#     else:
-#         print("False")
-# else:
-#     print("False")
+    print("False")
 
 # or
 
