5be7080eaf437f17027055b62c6877eabbdad17d
aurimas13 <aurimas.nausedas@gmail.com>
2022-05-21 14:38:57 +0300

    Added solution & description

--- HackerRank/Python Solutions/Array Mathematics/array_maths.py
+++ HackerRank/Python Solutions/Array Mathematics/array_maths.py
@@ -3,7 +3,7 @@ import numpy
 N, M = map(int, input().split())
 # for _ in range(N): # gives a list
 A = numpy.array([list(map(int, input().split())) for _ in range(N)]) # gives a list of list
-B = numpy.array([list(map(int, input().split())) for _ in range(N)]) # gives a list of listt
+B = numpy.array([list(map(int, input().split())) for _ in range(N)]) # gives a list of list
 print(numpy.add(A,B))
 print(numpy.subtract(A,B))
 print(numpy.multiply(A,B))
