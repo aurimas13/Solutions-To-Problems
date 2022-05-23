53008d583df0a02fde09576e843f949883654a54
aurimas13 <aurimas.nausedas@gmail.com>
2022-05-23 12:51:00 +0300

    Updated solution & added description

--- HackerRank/Python Solutions/Linear Algebra/linear.py
+++ HackerRank/Python Solutions/Linear Algebra/linear.py
@@ -3,3 +3,4 @@ import numpy
 N = int(input())
 A = numpy.array([list(map(float, input().split())) for _ in range(N)])
 print(numpy.around(numpy.linalg.det(A), decimals=11))
+
--- HackerRank/Python Solutions/Map and Lambda Function/fibonacci.py
+++ HackerRank/Python Solutions/Map and Lambda Function/fibonacci.py
@@ -24,3 +24,4 @@ if __name__ == '__main__':
 
 # fib = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377]
 # print(list(map(lambda x: x**3, fib[:int(input())])))
+
