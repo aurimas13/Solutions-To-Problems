bb95bbecd0b293650aba634023e40e8ed9fd9e3f
aurimas13 <aurimas.nausedas@gmail.com>
2022-05-29 01:14:05 +0300

    Added solution

--- LeetCode/Python Solutions/Palindrome Number/palindrome.py
+++ LeetCode/Python Solutions/Palindrome Number/palindrome.py
@@ -1,6 +1,12 @@
+#Solution:
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         if(str(x) == str(x)[::-1]):
             return True
         else:
             return False
+
+# Instantiating a class to return a value in a command line
+if __name__ == '__main__':
+    Solve = Solution.isPalindrome(12, 121) # 1234 gives False
+    print(Solve)
