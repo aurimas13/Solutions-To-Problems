0146a48991aa989bb6f0e37ffc51733796425a46
aurimas13 <aurimas.nausedas@gmail.com>
2022-05-29 02:31:00 +0300

    Added solution

--- LeetCode/Python Solutions/Two Sum/two_sum.py
+++ LeetCode/Python Solutions/Two Sum/two_sum.py
@@ -3,15 +3,19 @@ from typing import List
 from itertools import combinations
 class Solution:
     def twoSum(self, nums: List[int], target: int): #-> List[int]:
-        all = list(combinations(nums, 2))
-        for i in all:
-            if i[0] + i[1] == target:
-                return i[0], i[1]
+        all = combinations(nums, 2)
+        for i, v in enumerate(all):
+            print(v[0]+v[1])
+            if v[i] + v[i+1] == target:
+                return [i, i+1]
             else:
                 pass
+
 # Instantiation
 if __name__ == '__main__':
     Solve = Solution.twoSum(12, [2,7,11,15], 9)  # 1234 gives False
-    print(Solve)
+    Solve_1 = Solution.twoSum(12, [3,2,3], 6)  # 1234 gives False
+    Solve_2 = Solution.twoSum(12, [3,2,4], 7)  # 1234 gives False
+    print(Solve, Solve_1, Solve_2)
 
 
