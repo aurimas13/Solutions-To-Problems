7a88f9e876ffa7e625d24c3cf5b04312ce3ab1de
aurimas13 <aurimas.nausedas@gmail.com>
2022-05-30 19:23:57 +0300

    Updated solution

--- LeetCode/Python Solutions/Merge Two Sorted Lists/merge_sorted.py
+++ LeetCode/Python Solutions/Merge Two Sorted Lists/merge_sorted.py
@@ -38,6 +38,22 @@ class Solution:
             temp = temp.next
         return head
 
+# or
+#
+# head = sortedList = ListNode()
+# while l1 and l2 :
+# 	if l1.val <= l2.val :
+# 		sortedList.next = l1
+# 		l1 = l1.next
+# 		sortedList = sortedList.next
+#
+# 	else :
+# 		sortedList.next = l2
+# 		l2 = l2.next
+# 		sortedList = sortedList.next
+
+# sortedList.next = l1 or l2
+# return head.next
 
 # Instantiation of the class to check the values
 # if __name__ == '__main__':
