b417c7771d1a8a48401b67d970c6d9ac975d50b8
aurimas13 <aurimas.nausedas@gmail.com>
2022-06-04 16:06:22 +0300

    Added solution & description

--- LeetCode/Python Solutions/Valid Perfect Square/valid.py
+++ LeetCode/Python Solutions/Valid Perfect Square/valid.py
@@ -1,28 +1,37 @@
 from math import sqrt
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
-        x = num // 2
-        y = set([x])
-        while x * x != num:
-            if x == 0:
-                return True
-            else:
-                x = (x + (num // x)) // 2
-            if x in y: return False
-            y.add(x)
-        return True
+        if num < 2:
+            return True
 
-        # if num < 0:
-        #     return False
-        # if num == 0:
-        #     return True
-        # if num == 1:
-        #     return True
-        # if num > 1:
-        #     for i in range(0, num):
-        #         if i * i == num:
-        #             return True
-        # return False
+        left = 2
+        right = num // 2
+
+        while left <= right:
+            x = (left + right) // 2
+            guess_squared = x * x
+            if guess_squared == num:
+                return True
+            if guess_squared > num:
+                right = x - 1
+            else:
+                left = x + 1
+        return False
+
+        # or
+
+        # x = num // 2
+        # y = set([x])
+        # while x * x != num:
+        #     if x == 0:
+        #         return True
+        #     else:
+        #         x = (x + (num // x)) // 2
+        #     if x in y: return False
+        #     y.add(x)
+        # return True
+
+        # or
 
         # import math
         # if (num < 0):
