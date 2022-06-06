e80bd02f2e6a05c0f3dc63e53479a5f0febd7fde
aurimas13 <aurimas.nausedas@gmail.com>
2022-06-06 14:57:56 +0300

    Added solution & description

--- LeetCode/Python Solutions/Maximum Number of Balloons/maximum.py
+++ LeetCode/Python Solutions/Maximum Number of Balloons/maximum.py
@@ -1,9 +1,27 @@
+import math
 class Solution:
     def maxNumberOfBalloons(self, text: str) -> int:
+        bCount, aCount, lCount, oCount, nCount = 0, 0, 0, 0, 0
 
+        for i in range(len(text)):
+            if text[i] == 'b':
+                bCount += 1
+            elif text[i] == 'a':
+                aCount+= 1
+            elif text[i] == 'l':
+                lCount+= 1
+            elif text[i] == 'o':
+                oCount+= 1
+            elif text[i] == 'n':
+                nCount+= 1
+
+        lCount = lCount / 2;
+        oCount = oCount / 2;
+
+        return math.floor(min(bCount, aCount, lCount, oCount, nCount))
 
 # Checking in console
 if __name__ == '__main__':
     Instant = Solution()
-    Solve = Instant.merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)  # empty output - None
+    Solve = Instant.maxNumberOfBalloons('balon')  # nlaebolko -> 1 | loonbalxballpoon -> 2
     print(Solve)
