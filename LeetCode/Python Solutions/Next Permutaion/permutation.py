2ffa154353bc3538e5c182928068ee192222e177
aurimas13 <aurimas.nausedas@gmail.com>
2022-06-07 17:42:42 +0300

    Added solution & description

--- LeetCode/Python Solutions/Next Permutaion/permutation.py
+++ LeetCode/Python Solutions/Next Permutaion/permutation.py
@@ -23,7 +23,7 @@ class Solution:
             j -= 1
 
 
-# Checking in PyCharm
+# Checking in console
 if __name__ == '__main__':
     Instant = Solution()
     Solve = Instant.nextPermutation([1, 2, 3])  # [1, 2, 3] = [1, 3, 2], [1, 1, 5] -> [1, 5, 1]
