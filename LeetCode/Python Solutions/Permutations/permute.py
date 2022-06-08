7c05ab5799f0688b6830c41932223db93a3c83d1
aurimas13 <aurimas.nausedas@gmail.com>
2022-06-08 10:25:35 +0300

    Added solution & description

--- LeetCode/Python Solutions/Permutations/permute.py
+++ LeetCode/Python Solutions/Permutations/permute.py
@@ -3,3 +3,11 @@ from itertools import permutations
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         return list(permutations(nums))
+
+
+# Checking in console
+if __name__ == '__main__':
+    Instant = Solution()
+    Solve = Instant.permute([1, 2, 3])  # [1,2,3] -> [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+    print(Solve)
+
