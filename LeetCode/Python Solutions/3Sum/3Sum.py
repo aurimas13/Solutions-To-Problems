38b7cd2556ca6dfc371d488881593b9d38ce390a
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2022-07-20 13:24:40 +0300

    Added solution & description

--- LeetCode/Python Solutions/3Sum/3Sum.py
+++ LeetCode/Python Solutions/3Sum/3Sum.py
@@ -1,11 +1,20 @@
 from typing import List
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # Setting two sets; one for storing results the other for storing duplicates
         res, dups = set(), set()
+        # Enumerating nums list and searching for all values of it that sum to 0
         for i, val1 in enumerate(nums):
+            # Initializing a new set for that particular index and val1
             seen = set()
+            # Looking if val1 is unique and addition of other values give a zero
             if val1 not in dups:
                 dups.add(val1)
+				# Looping over val2 in nums list of one bigger
+				# looking whether a value exists in a seen list
+				# and if it does it means we can have triplets that sum to zero
+				# and add this to res set as a possible solution
+				# return the tuple of values that sum to zero
                 for val2 in nums[i+1:]:
                     complement = -val1 - val2
                     if complement in seen:
