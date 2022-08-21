3d3927a969f5ede7c0a9a69671c8f16124b84108
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2022-08-21 08:12:50 +0300

    Added solution & description

---
+++ LeetCode/Python Solutions/Unique Paths/README.md
@@ -0,0 +1,1 @@
+Problem description of "Unique Paths" can be found [here](https://leetcode.com/problems/unique-paths/).
---
+++ LeetCode/Python Solutions/Unique Paths/unique.py
@@ -0,0 +1,21 @@
+from typing import List
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        def paths(x,y):
+            if (x,y) in mem:
+                return mem[(x,y)]
+            if x == 1 and y == 1:
+                return 1
+            if x < 1 or y < 1:
+                return 0
+            mem[(x,y)] = paths(x,y-1) + paths(x-1,y)
+            return mem[(x,y)]
+        mem = {}
+        return paths(m,n)
+
+
+
+if __name__ == '__main__':
+    Instant = Solution()
+    Solve = Instant.uniquePaths( = 3, n = 7) # m = 3, n = 2 -> 3 | m = 3, n = 7 -> 28
+    print(Solve)
