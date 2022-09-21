63442751b5130e81fa6cfe047c52e2d3f6051048
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2022-09-21 07:53:57 +0300

    Added solution & description

---
+++ LeetCode/Python Solutions/Random Pick with Weight/README.md
@@ -0,0 +1,1 @@
+Problem description of "Random Pick with Weight" can be found [here](https://leetcode.com/problems/random-pick-with-weight/).
---
+++ LeetCode/Python Solutions/Random Pick with Weight/random.py
@@ -0,0 +1,24 @@
+from typing import List
+import random
+class Solution:
+
+    def __init__(self, w: List[int]):
+        curr = 0
+        self.prefix_sums = [curr := curr + i for i in w]
+        self.sum = curr
+
+    def pickIndex(self) -> int:
+        res = self.sum * random.random()
+        l, r = 0, len(self.prefix_sums)
+        while l < r:
+            m = l + (r - l) // 2
+            if res > self.prefix_sums[m]:
+                l = m + 1
+            else:
+                r = m
+        return l
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
