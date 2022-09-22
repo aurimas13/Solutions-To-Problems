880046bdd4324d32fe66eef981ddf57c04a0da21
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2022-09-22 08:31:50 +0300

    Added solution & description

--- LeetCode/Python Solutions/H-Index/index.py
+++ LeetCode/Python Solutions/H-Index/index.py
@@ -1,6 +1,7 @@
 from typing import List
-
 # Sorting complexity solution: Time & Space - O(n)
+
+
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
         citations_length = len(citations)
@@ -8,9 +9,9 @@ class Solution:
 
         # Counting papers for each citation number
         for citation in citations:
-        # Any citation larger than n can be
-        # replaced by citations_length and the h-index will
-        # not change after the replacement:
+            # Any citation larger than n can be
+            # replaced by citations_length and the h-index will
+            # not change after the replacement:
             index = min(citations_length, citation)
             papers[index] += 1
 
@@ -26,6 +27,5 @@ class Solution:
 # Running in terminal/console:
 if __name__ == '__main__':
     Instant = Solution()
-    Solve = Instant.hIndex(citations = [3,0,6,1,5])  # citations = [3,0,6,1,5] -> 3 | citations = [1,3,1] -> 1
+    Solve = Instant.hIndex(citations=[3, 0, 6, 1, 5])  # citations = [3,0,6,1,5] -> 3 | citations = [1,3,1] -> 1
     print(Solve)
-
