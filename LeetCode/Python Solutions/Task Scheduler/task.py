93d0259700c168acde90c75e493c758ef23cfb34
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2022-09-24 06:21:33 +0300

    Added solution & description

--- LeetCode/Python Solutions/Task Scheduler/task.py
+++ LeetCode/Python Solutions/Task Scheduler/task.py
@@ -1,19 +1,30 @@
 from typing import List
+import heapq
+
+
 class Solution:
-    def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        dp = {}
-        def comb(idx, running_sum):
-            if idx + 1 == len(nums):
-                if running_sum == target:
-                    return 1
-                return 0
-            if (idx, running_sum) not in dp:
-                dp[(idx, running_sum)] = comb(idx + 1, running_sum + nums[idx +1]) + comb(idx + 1, running_sum - nums[idx + 1])
-            return dp[(idx, running_sum)]
-        return comb(-1, 0)
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        task_frequencies = self.tasksToDic(tasks, {})
+        max_heap = []
+        for value in task_frequencies.values():
+            heapq.heappush(max_heap, -1 * value)
+        max_frequency = -1 * heapq.heappop(max_heap)
+        max_idle_time = (max_frequency - 1) * n
+        while max_heap:
+            max_idle_time -= min(max_frequency - 1, -1 * heapq.heappop(max_heap))
+        max_idle_time = max(max_idle_time, 0)
+        return len(tasks) + max_idle_time
+
+    def tasksToDic(self, tasks, dic):
+        for task in tasks:
+            if task not in dic:
+                dic[task] = 1
+            else:
+                dic[task] += 1
+        return dic
 
 
 if __name__ == '__main__':
     Instant = Solution()
-    Solve = Instant.findTargetSumWays(nums = [1,1,1,1,1], target = 3) # nums = [1,1,1,1,1], target = 3 -> 5
+    Solve = Instant.leastInterval(tasks = ["A","A","A","B","B","B"], n = 2) # tasks = ["A","A","A","B","B","B"], n = 2 -> 8 | tasks = ["A","A","A","B","B","B"], n = 0 -> 6
     print(Solve)
