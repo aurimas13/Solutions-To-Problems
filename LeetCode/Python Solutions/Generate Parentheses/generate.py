eca741d091f5a3560ae63e142585ab0b812a3b3e
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2022-10-08 09:11:08 +0100

    Added solution & description

--- LeetCode/Python Solutions/Generate Parentheses/generate.py
+++ LeetCode/Python Solutions/Generate Parentheses/generate.py
@@ -1,28 +1,21 @@
 from typing import List
-from collections import deque
 class Solution:
-    def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        if len(edges) < n - 1:
-            return False
-        adj = [set() for _ in range(n)]
-        for i, j in edges:
-            adj[i].add(j)
-            adj[j].add(i)
-        queue = deque([i for i in range(n) if len(adj[i]) == 1])
-        while queue:
-            i = queue.popleft()
-            if not adj[i]:
-                continue
-            nb = adj[i].pop()
-            adj[nb].discard(i)
-            if len(adj[nb]) == 1:
-                queue.append(nb)
-
-        return all([len(x) == 0 for x in adj])
+    def generateParenthesis(self, n: int) -> List[str]:
+        immediate = [{""}]
+        for i in range(1, n + 1):
+            cur = set()
+            for each in immediate[-1]:
+                cur.add("(" + each + ")")
+            for j in range(1, i):
+                for l in immediate[j]:
+                    for r in immediate[i - j]:
+                        cur.add(l + r)
+            immediate.append(cur)
+        return list(immediate[-1])
 
 
 # Checking in terminal/console:
-if __name__ == '__main__':
+if __name__ == '__main__':cd
     Sol = Solution()
-    Solve = Sol.validTree(n = 5, edges = [[0,1],[0,2],[0,3],[1,4]])  # n = 5, edges = [[0,1],[0,2],[0,3],[1,4]] -> true | n = 5, edges = [[0,1],[1,2],[2,3],[1,3],[1,4]] -> false
+    Solve = Sol.generateParenthesis(n=3)  # n=1 -> ["()"] | n=3 -> ["((()))","(()())","(())()","()(())","()()()"]
     print(Solve)
