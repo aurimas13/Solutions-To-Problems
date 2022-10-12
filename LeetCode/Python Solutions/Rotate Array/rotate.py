b6c0880cca0d3334d57b3449e3cda44948fe234d
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2022-10-12 08:44:07 +0100

    Added solution

--- LeetCode/Python Solutions/Rotate Array/rotate.py
+++ LeetCode/Python Solutions/Rotate Array/rotate.py
@@ -1,15 +1,13 @@
 from typing import List
 class Solution:
-    def rotate(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
-        matrix[:] = [[matrix[-j-1][i] for j in range(len(matrix))] for i in range(len(matrix[0]))]
-        return (matrix[:]) # Delete this when running in LeetCode
+    def rotate(self, nums: List[int], k: int) -> None:
+        pivot = len(nums) - k % len(nums)
+        nums[:] = nums[pivot:] + nums[:pivot]
+        return nums[:] # Delete this when running in Leetcode
 
 
 # Checking in PyCharm/terminal:
 if __name__ == '__main__':
     Instant = Solution()
-    Solve = Instant.rotate([[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]])  #  matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]] ->  [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
+    Solve = Instant.rotate(nums = [-1,-100,3,99], k = 2)  # nums = [-1,-100,3,99], k = 2 -> [3,99,-1,-100]
     print(Solve)
