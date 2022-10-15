4f5809115f477795051754aa097d54c6931a8591
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2022-10-15 09:33:54 +0100

    Added solution & description

---
+++ LeetCode/Python Solutions/Valid Parenthesis String/README.md
@@ -0,0 +1,1 @@
+Problem description of "Simplify Path" can be found [here](https://leetcode.com/problems/simplify-path/) and its solution [here]().
---
+++ LeetCode/Python Solutions/Valid Parenthesis String/valid.py
@@ -0,0 +1,29 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        # CC O(n), SC O(n)
+        stack = []
+        star = []
+
+        for i, char in enumerate(s):
+            if char == '(':
+                stack.append(i)
+            elif char == ')':
+                if stack:
+                    stack.pop()
+                elif star:
+                    star.pop()
+                else:
+                    return False
+            else:
+                star.append(i)
+
+        while star and stack and star[-1] > stack[-1]:
+            star.pop()
+            stack.pop()
+        return len(stack) == 0
+
+
+if __name__ == '__main__':
+    Instant = Solution()
+    Solve = Instant.checkValidString(s = "(*))")  # s = "(*))" -> true
+    print(Solve)
