b4880bdd955cda25d493d34c3a2d9607bd1b76a2
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2022-11-01 05:24:12 +0200

    Updated solution & description

--- LeetCode/Python Solutions/Sqrt(x)/sqrt_x.py
+++ LeetCode/Python Solutions/Sqrt(x)/sqrt_x.py
@@ -1,18 +1,16 @@
 import math
 class Solution:
     def mySqrt(self, x: int) -> int:
-        class Solution:
-            def mySqrt(self, x):
-                if x < 2:
-                    return x
+        if x < 2:
+            return x
 
-                x0 = x
-                x1 = (x0 + x / x0) / 2
-                while abs(x0 - x1) >= 1:
-                    x0 = x1
-                    x1 = (x0 + x / x0) / 2
+        x0 = x
+        x1 = (x0 + x / x0) / 2
+        while abs(x0 - x1) >= 1:
+            x0 = x1
+            x1 = (x0 + x / x0) / 2
 
-                return int(x1)
+        return int(x1)
 
 # or
 # return math.floor(math.sqrt(x))
