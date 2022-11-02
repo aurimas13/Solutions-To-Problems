69a4543157f04872340d1e8717cb0a9d2f7e0ae5
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2022-11-02 05:13:14 +0200

    Updated description

--- LeetCode/Python Solutions/K Closest Points to Origin/closest.py
+++ LeetCode/Python Solutions/K Closest Points to Origin/closest.py
@@ -1,16 +1,27 @@
+import heapq
 from typing import List
 
 
 class Solution:
     @staticmethod
-    def findKthLargest(nums: List[int], k: int) -> int:
-        n = sorted(nums)
-        l = len(nums)
-        return n[l-k]
+    def kClosest(points: List[List[int]], k: int) -> List[List[int]]:
+        min_heap = []
+        i = 0
+        res = []
+        for n1, n2 in points:
+            dist = (n1**2) + (n2**2)
+            heapq.heappush(min_heap, (dist, n1, n2))
+
+        while i < k:
+            dist, n1, n2 = heapq.heappop(min_heap)
+            res.append([n1, n2])
+            i += 1
+        return res
 
 
 # Checking in PyCharm/terminal
 if __name__ == '__main__':
     Instant = Solution()
-    Solve = Instant.findKthLargest(nums=[3, 2, 1, 5, 6, 4], k=2)  #  nums = [3,2,1,5,6,4], k = 2 -> 5 | nums = [3,2,3,1,2,4,5,5,6], k = 4 -> 4
+    Solve = Instant.kClosest(points = [[3, 3],[5, -1],[-2, 4]], k=2)  # points = [[3,3],[5,-1],[-2,4]], k = 2 -> [[3,
+    # 3],[-2,4]] | points = [[1,3],[-2,2]], k = 1 -> [[-2,2]]
     print(Solve)
