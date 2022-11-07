679c4d6cadb777c2659c514dea71355290485ae3
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2022-11-07 07:15:25 +0200

    Updated solution

--- LeetCode/Python Solutions/Subsets/subsets.py
+++ LeetCode/Python Solutions/Subsets/subsets.py
@@ -1,12 +1,17 @@
 class Solution:
-    def getSum(self, a: int, b: int) -> int:
-        values = []
-        values.append(a)
-        values.append(b)
-        return sum(values)
+    @staticmethod
+    def subsets(nums: List[int]) -> List[List[int]]:
+        answer = []
+
+        for i in range(2**len(nums)):
+            subset = [nums[j] for j in range(len(nums)) if (1 << j) & i]
+            answer.append(subset)
+
+        return answer
 
 
 if __name__ == '__main__':
     Instant = Solution()
-    Solve = Instant.getSum(2, 3) # 2, 3 -> 5 | a = 1, b = 2 -> 3
+    Solve = Instant.subsets(nums = [1,2,3]) # nums = [1,2,3] ->
+    # [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]] | nums = [0] -> [[],[0]]
     print(Solve)
