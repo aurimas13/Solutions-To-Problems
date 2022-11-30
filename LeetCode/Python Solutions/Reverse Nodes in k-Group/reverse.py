92c76ef9c62e0c83f1e8df737808169f8f0826d9
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2022-11-30 13:19:36 +0200

    Updated solution

--- LeetCode/Python Solutions/Reverse Nodes in k-Group/reverse.py
+++ LeetCode/Python Solutions/Reverse Nodes in k-Group/reverse.py
@@ -1,15 +1,40 @@
 class Solution:
-    @staticmethod
-    def reverseWords(s: str) -> str:
-        result = ''
-        for j in s.split():
-            result = result + j[::-1] + ' '
-        return result.rstrip()
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        new = ListNode()
+        new.next = head
 
+        curr = head
+        prev = new
+        last_end = new
+        is_valid = True
 
-if __name__ == '__main__':
-    Instant = Solution()
-    Solve = Instant.reverseWords(s = "Let's take LeetCode contest")
-    print(Solve)
-    # s = "Let's take LeetCode contest" -> "s'teL ekat edoCteeL tsetnoc"
+        while curr:
 
+            counter = 1
+            checker = curr
+
+            while counter < k:
+                checker = checker.next
+                if checker is None:
+                    is_valid = False
+                    break
+                counter += 1
+
+            if not is_valid:
+                break
+
+            counter = 0
+
+            while counter < k:
+                temp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = temp
+                counter += 1
+
+            swap_end = last_end.next
+            last_end.next = prev
+            swap_end.next = curr
+            last_end = swap_end
+
+        return new.next
