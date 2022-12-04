4924bb3dd79a77949c51b6573712d56f02b5adb2
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2022-12-04 09:17:15 +0200

    Updated solution

--- LeetCode/Python Solutions/Sliding Window Maximum/sliding.py
+++ LeetCode/Python Solutions/Sliding Window Maximum/sliding.py
@@ -28,7 +28,7 @@ class Solution:
 # Checking in console
 if __name__ == '__main__':
     Instant = Solution()
-    Solve = Instant.maxSlidingWindow([4,1,2,1,2])
+    Solve = Instant.maxSlidingWindow(nums = [1,3,-1,-3,5,3,6,7], k = 3 )
     # nums = [1,3,-1,-3,5,3,6,7], k = 3 -> [3,3,5,5,6,7]
     # nums = [1], k = 1 -> [1]
     print(Solve)
