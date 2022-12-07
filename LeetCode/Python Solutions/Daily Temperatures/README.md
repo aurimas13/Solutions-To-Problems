9b318afb481aa76b2e5dd1c00b640f62ffcd5834
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2022-12-07 05:26:34 +0200

    Added description and solution

---
+++ LeetCode/Python Solutions/Daily Temperatures/README.md
@@ -0,0 +1,1 @@
+Problem description of "Daily Temperatures" can be found [here](https://leetcode.com/problems/daily-temperatures/description/) and its solution [here]().
---
+++ LeetCode/Python Solutions/Daily Temperatures/daily.py
@@ -0,0 +1,22 @@
+from typing import List
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        n = len(temperatures)
+        ans = [0] * n
+        stack = []
+        for i, temp in enumerate(temperatures):
+            while stack and temperatures[stack[-1]] < temp:
+                left = stack.pop()
+                ans[left] = i - left
+            stack.append(i)
+        return ans
+
+
+# Checking in terminal/console:
+if __name__ == '__main__':
+    Sol = Solution()
+    Solve = Sol.dailyTemperatures(temperatures = [73,74,75,71,69,72,76,73])
+    # temperatures = [73,74,75,71,69,72,76,73] -> [1,1,4,2,1,1,0,0]
+    #  temperatures = [30,40,50,60] -> [1,1,1,0]
+    print(Solve)
