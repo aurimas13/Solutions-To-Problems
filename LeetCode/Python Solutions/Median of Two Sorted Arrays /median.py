44721a39086add92b46e14c3e07187378af3766f
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2022-12-11 07:44:42 +0200

    Updated solution

--- LeetCode/Python Solutions/Median of Two Sorted Arrays /median.py
+++ LeetCode/Python Solutions/Median of Two Sorted Arrays /median.py
@@ -1,15 +1,32 @@
-from typing import List
 class Solution:
-    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
-        intervals.sort()
-        for i in range(len(intervals) - 1):
-            if intervals[i][1] > intervals[i + 1][0]:
-                return False
-        return True
+    @staticmethod
+    def findMedianSortedArrays(nums1: List[int], nums2: List[int]) -> float:
+        n = [0] * (len(nums1) + len(nums2))
+        li, ri, i = 0, 0, 0
+        while (li < len(nums1) and ri < len(nums2)):
+            if nums1[li] < nums2[ri]:
+                n[i] = nums1[li]
+                li += 1
+            else:
+                n[i] = nums2[ri]
+                ri += 1
+            i += 1
+
+        if li != len(nums1):
+            n[i:] = nums1[li:]
+        else:
+            n[i:] = nums2[ri:]
+        m = len(n)
+
+        if len(n) % 2 != 0:
+            return float(n[(m + 1) // 2 - 1])
+        return (n[(m // 2 - 1)] + n[(m // 2 + 1 - 1)]) / 2
 
 
-# Checking in PyCharm/terminal:
+# Checking in terminal/console:
 if __name__ == '__main__':
-    Instant = Solution()
-    Solve = Instant.canAttendMeetings([[7,10],[2,4]])  # intervals = [[7,10],[2,4]] -> true | [[0,30],[5,10],[15,20]] -> false
+    Sol = Solution()
+    Solve = Sol.findMedianSortedArrays(nums1 = [1,3], nums2 = [2])
+    # nums1 = [1,3], nums2 = [2] -> 2.00000
+    # nums1 = [1,2], nums2 = [3,4] -> 2.50000
     print(Solve)
