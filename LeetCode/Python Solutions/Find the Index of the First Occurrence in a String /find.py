da8f77c562db069ed67cbcfb4c5331c0b617b0a3
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2022-12-11 07:41:40 +0200

    Added solution & description

--- LeetCode/Python Solutions/Find the Index of the First Occurrence in a String /find.py
+++ LeetCode/Python Solutions/Find the Index of the First Occurrence in a String /find.py
@@ -1,23 +1,32 @@
-from typing import List
-
-
 class Solution:
     @staticmethod
-    def strStr(haystack: str, needle: str) -> int:
-        m = len(haystack)
-        n = len(needle)
+    def findMedianSortedArrays(nums1: List[int], nums2: List[int]) -> float:
+        n = [0] * (len(nums1) + len(nums2))
+        li, ri, i = 0, 0, 0
+        while (li < len(nums1) and ri < len(nums2)):
+            if nums1[li] < nums2[ri]:
+                n[i] = nums1[li]
+                li += 1
+            else:
+                n[i] = nums2[ri]
+                ri += 1
+            i += 1
 
-        for i in range(m - n + 1):
-            if haystack[i:i + n] == needle:
-                return i
+        if li != len(nums1):
+            n[i:] = nums1[li:]
+        else:
+            n[i:] = nums2[ri:]
+        m = len(n)
 
-        return -1
+        if len(n) % 2 != 0:
+            return float(n[(m + 1) // 2 - 1])
+        return (n[(m // 2 - 1)] + n[(m // 2 + 1 - 1)]) / 2
 
 
 # Checking in terminal/console:
 if __name__ == '__main__':
     Sol = Solution()
-    Solve = Sol.strStr(haystack = "sadbutsad", needle = "sad")
-    # haystack = "sadbutsad", needle = "sad" -> 0
-    # haystack = "leetcode", needle = "leeto" -> -1
+    Solve = Sol.findMedianSortedArrays(nums1 = [1,3], nums2 = [2])
+    # nums1 = [1,3], nums2 = [2] -> 2.00000
+    # nums1 = [1,2], nums2 = [3,4] -> 2.50000
     print(Solve)
---
+++ LeetCode/Python Solutions/Median of Two Sorted Arrays /README.md
@@ -0,0 +1,1 @@
+Problem description of "Median of Two Sorted Arrays " can be found [here](https://leetcode.com/problems/median-of-two-sorted-arrays/description/) and its solution [here]().
