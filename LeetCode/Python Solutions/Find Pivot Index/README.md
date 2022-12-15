fb3fd58a674d6e65e4f06c83786c01fdef384a0f
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2022-12-15 08:03:24 +0200

    Added solution & description

---
+++ LeetCode/Python Solutions/Find Pivot Index/README.md
@@ -0,0 +1,1 @@
+Problem description of "Find Pivot Index" can be found [here](https://leetcode.com/problems/find-pivot-index/solutions/2913430/python-solution/) and its solution [here]().
---
+++ LeetCode/Python Solutions/Find Pivot Index/find.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def pivotIndex(nums: List[int]) -> int:
+        for i in range(len(nums)):
+            if sum(nums[:i]) == sum(nums[i + 1:]):
+                return i
+        return -1
+
+
+# Checking in terminal/console:
+if __name__ == '__main__':
+    Sol = Solution()
+    Solve = Sol.pivotIndex([11, 13, 15, 17])
+    # nums = [1,7,3,6,5,6] -> 3
+    # nums = [1,2,3] -> -1
+    print(Solve)
