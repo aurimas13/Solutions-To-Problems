e96c83e1feb68f593d7e2ac89ca40398dde48332
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2022-12-27 12:00:43 +0200

    Updated solution

--- LeetCode/Python Solutions/Search a 2D Matrix/search.py
+++ LeetCode/Python Solutions/Search a 2D Matrix/search.py
@@ -23,3 +23,4 @@ if __name__ == '__main__':
     Solve = Instant.searchMatrix(matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3 )
     # matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]], target = 3 -> true
     print(Solve)
+
