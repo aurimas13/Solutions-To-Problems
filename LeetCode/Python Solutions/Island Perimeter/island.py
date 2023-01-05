5368a492e2a9b1a60d6168910f8aee518a39babb
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-01-05 23:18:59 +0200

    Updated solution

--- LeetCode/Python Solutions/Island Perimeter/island.py
+++ LeetCode/Python Solutions/Island Perimeter/island.py
@@ -1,24 +1,39 @@
+from typing import List
+from collections import deque
+
+
 class Solution:
     @staticmethod
-    def isIsomorphic(s: str, t: str) -> bool:
-        l={}
-        if len(s)!=len(t):
-            return False
-        for i in range(len(s)):
-            c1,c2=s[i],t[i]
-            if c1 not in l:
-                if c2 in l.values():
-                    return False
-                l[c1]=c2
-            elif l[c1]!=c2:
-                return False
-        return True
+    def islandPerimeter(grid: List[List[int]]) -> int:
+        search_queue = deque()
+        visited = set()
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    visited.add((i,j))
+                    search_queue.append((i,j))
+        perimeter = 0
+
+        while search_queue:
+            k, l = search_queue.popleft()
+            count = 4
+            if (k+1, l) in visited:
+                count-=1
+            if (k-1, l) in visited:
+                count-=1
+            if (k, l+1) in visited:
+                count-=1
+            if (k, l-1) in visited:
+                count-=1
+            perimeter+=count
+        return perimeter
 
 
 # Running in terminal/console:
 if __name__ == '__main__':
     Instant = Solution()
-    Solve = Instant.isIsomorphic(s = "egg", t = "add")
-    # s = "egg", t = "add" -> true
-    # s = "foo", t = "bar" -> false
+    Solve = Instant.islandPerimeter(grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]])
+    # grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]] -> 16
+    # grid = [[1]] -> 4
+    # grid = [[1,0]] -> 4
     print(Solve)
