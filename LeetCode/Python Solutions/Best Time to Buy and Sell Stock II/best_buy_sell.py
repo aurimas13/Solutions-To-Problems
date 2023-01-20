b44e19d9da323f9676f6d72a4631c8b37ed479b7
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-01-20 17:49:51 +0200

    Updated solution

--- LeetCode/Python Solutions/Best Time to Buy and Sell Stock II/best_buy_sell.py
+++ LeetCode/Python Solutions/Best Time to Buy and Sell Stock II/best_buy_sell.py
@@ -1,19 +1,19 @@
 from typing import List
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        i = 0;
-        valley = prices[0];
-        peak = prices[0];
-        maxprofit = 0;
+        i = 0
+        valley = prices[0]
+        peak = prices[0]
+        maxprofit = 0
         while i < len(prices) - 1:
             while (i < len(prices) - 1 and prices[i] >= prices[i + 1]):
-                i += 1;
-            valley = prices[i];
+                i += 1
+            valley = prices[i]
             while (i < len(prices) - 1 and prices[i] <= prices[i + 1]):
-                i += 1;
-            peak = prices[i];
-            maxprofit += peak - valley;
-        return maxprofit;
+                i += 1
+            peak = prices[i]
+            maxprofit += peak - valley
+        return maxprofit
 
 
 # Checking in PyCharm/console:
