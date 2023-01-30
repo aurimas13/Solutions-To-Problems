062a1f34daaca9ffe49794e31dcb903e8682bf40
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-01-30 07:57:41 +0200

    Added solution & description

--- LeetCode/Python Solutions/X of a Kind in a Deck of Cards/README.md
+++ LeetCode/Python Solutions/X of a Kind in a Deck of Cards/README.md
@@ -1,1 +1,3 @@
-Problem description of "X of a Kind in a Deck of Cards" can be found [here](https://leetcode.com/problems/x-of-a-kind-in-a-deck-of-cards/).
+Problem description of "X of a Kind in a Deck of Cards" can be found [here](https://leetcode.com/problems/x-of-a-kind-in-a-deck-of-cards/)
+and the solution [here](https://github.com/aurimas13/Solutions-To-Problems/blob/main/LeetCode/Python%20Solutions/X%20of%20a%20Kind%20in%20a%20Deck%20of%20Cards/deck.py)
+as well as explanation with solution [here](https://leetcode.com/problems/x-of-a-kind-in-a-deck-of-cards/solutions/2258224/fast-python-solution/).
--- LeetCode/Python Solutions/X of a Kind in a Deck of Cards/deck.py
+++ LeetCode/Python Solutions/X of a Kind in a Deck of Cards/deck.py
@@ -2,13 +2,17 @@ from typing import List
 from collections import Counter
 from math import gcd
 from functools import reduce
+
+
 class Solution:
-    def hasGroupsSizeX(self, deck: List[int]) -> bool:
+    @staticmethod
+    def hasGroupsSizeX(deck: List[int]) -> bool:
         vals = Counter(deck).values()
         return reduce(gcd, vals) >= 2
 
 
 if __name__ == '__main__':
     Instant = Solution()
-    Solve = Instant.hasGroupsSizeX([1,2,3,4,4,3,2,1]) # [1,2,3,4,4,3,2,1] -> true | deck = [1,1,1,2,2,2,3,3] -> false
+    Solve = Instant.hasGroupsSizeX([1,2,3,4,4,3,2,1])
+    # [1,2,3,4,4,3,2,1] -> true | deck = [1,1,1,2,2,2,3,3] -> false
     print(Solve)
