cc4f547c2bfa0a2b60c23f8bd5b29ce78a6a2e16
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-02-20 07:49:38 +0200

    Added solution & description

--- LeetCode/Python Solutions/Path with Maximum Gold/README.md
+++ LeetCode/Python Solutions/Path with Maximum Gold/README.md
@@ -1,3 +1,3 @@
 Problem description of "Path with Maximum Gold" can be found 
-[here](https://leetcode.com/problems/path-with-maximum-gold/description/) and its solution [here]()
+[here](https://leetcode.com/problems/path-with-maximum-gold/description/) and its solution [here](https://github.com/aurimas13/Solutions-To-Problems/blob/main/LeetCode/Python%20Solutions/Path%20with%20Maximum%20Gold/path.py)
 while the explanationa and solution [here](https://leetcode.com/problems/path-with-maximum-gold/solutions/3208526/python-solution/).
--- LeetCode/Python Solutions/Path with Maximum Gold/path.py
+++ LeetCode/Python Solutions/Path with Maximum Gold/path.py
@@ -1,22 +1,32 @@
 from typing import List
+
+
 class Solution:
-    def getRow(self, rowIndex: int) -> List[int]:
-        if rowIndex == 0:
-            return [1]
-        elif rowIndex == 1:
-            return [1, 1]
-        Triangle = [[1]]
-        for i in range(1, rowIndex+1):
-            row = [1]
-            for j in range(1, i):
-                row.append(Triangle[i - 1][j - 1] + Triangle[i - 1][j])
-            row.append(1)
-            Triangle.append(row)
-        return row
+    def getMaximumGold(self, grid: List[List[int]]) -> int:
+        max_gold = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] > 0:
+                    max_gold = max(max_gold, self.explore(grid, i, j))
+        return max_gold
+    
+    def explore(self, grid, i, j):
+        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 0:
+            return 0
+        gold = grid[i][j]
+        grid[i][j] = 0  # mark as visited
+        max_gold = 0
+        for x, y in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
+            max_gold = max(max_gold, self.explore(grid, i+x, j+y))
+        grid[i][j] = gold  # backtrack
+        return max_gold + gold
+
 
 
 # Checking in PyCharm/terminal:
 if __name__ == '__main__':
     Instant = Solution()
-    Solve = Instant.getRow(3)  # 3 -> [1,3,3,1]
+    Solve = Instant.getRow(3)  
+    # grid = [[0,6,0],[5,8,7],[0,9,0]] -> 24
+    # grid = [[1,0,7],[2,0,6],[3,4,5],[0,3,0],[9,0,20]] -> 28
     print(Solve)
