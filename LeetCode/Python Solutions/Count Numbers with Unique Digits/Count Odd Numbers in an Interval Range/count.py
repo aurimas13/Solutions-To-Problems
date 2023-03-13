b5e671d37b8753d1773cb6c90cd8fbc796711195
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-03-13 06:17:02 +0200

    Added solution & description

---
+++ LeetCode/Python Solutions/Count Numbers with Unique Digits/Count Odd Numbers in an Interval Range/count.py
@@ -0,0 +1,24 @@
+class Solution:
+    def countNumbersWithUniqueDigits(self, n: int) -> int:
+        if n == 0:
+            return 1
+        
+        if n > 10:
+            n = 10
+            
+        count = 10
+        unique_digits = 9
+        
+        for i in range(2, n + 1):
+            unique_digits *= (11 - i)
+            count += unique_digits
+            
+        return count
+
+# Checking in Terminal/Console:
+if __name__ == '__main__':
+    Sol = Solution()
+    Solve = Sol.countNumbersWithUniqueDigits(n = 2)
+    # n = 2 -> 91
+    # n = 0 -> 1
+    print(Solve)
