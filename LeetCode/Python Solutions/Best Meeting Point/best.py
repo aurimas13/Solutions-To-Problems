c72a09666fb2190af61e2822ec29b530dae1647e
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-04-03 05:15:50 +0300

    Added solution & description

--- LeetCode/Python Solutions/Best Meeting Point/best.py
+++ LeetCode/Python Solutions/Best Meeting Point/best.py
@@ -1,19 +1,23 @@
 from typing import List
+
+
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        min_price = float('inf')
-        max_profit = 0
-        for i in range(len(prices)):
-            if prices[i] < min_price:
-                min_price = prices[i]
-            elif prices[i] - min_price > max_profit:
-                max_profit = prices[i] - min_price
+    def minTotalDistance(self, grid: List[List[int]]) -> int:
+        # Get the list of all row and column indices with people
+        rows = [i for i in range(len(grid)) for j in range(len(grid[0])) if grid[i][j] == 1]
+        cols = [j for i in range(len(grid)) for j in range(len(grid[0])) if grid[i][j] == 1]
 
-        return max_profit
+        # Calculate the median of rows and cols
+        row_median = sorted(rows)[len(rows) // 2]
+        col_median = sorted(cols)[len(cols) // 2]
 
+        # Calculate the minimum total distance
+        return sum(abs(row - row_median) for row in rows) + sum(abs(col - col_median) for col in cols)
 
-# Checking in PyCharm/console:
-if __name__ == '__main__':
-    Sol = Solution()
-    Solve = Sol.maxProfit([7,1,5,3,6,4])  # [7,1,5,3,6,4] -> 5
-    print(Solve)
+# Test cases
+solution = Solution()
+grid1 = [[1, 0, 0, 0, 1], [0, 0, 0, 0, 0], [0, 0, 1, 0, 0]]
+grid2 = [[1, 1, 0], [1, 0, 1], [0, 0, 1]]
+
+print(solution.minTotalDistance(grid1))  # Output: 6
+print(solution.minTotalDistance(grid2))  # Output: 4
