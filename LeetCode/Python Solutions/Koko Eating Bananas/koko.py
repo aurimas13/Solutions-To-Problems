c84d10a50e9baa8ed74a984bb9a79e5a648c439f
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-04-21 00:29:06 +0300

    Added solution & description

--- LeetCode/Python Solutions/Koko Eating Bananas/koko.py
+++ LeetCode/Python Solutions/Koko Eating Bananas/koko.py
@@ -1,33 +1,30 @@
 from typing import List
-from collections import defaultdict
-from collections import deque
-
 
 class Solution:
-    @staticmethod
-    def killProcess(pid: List[int], ppid: List[int], kill: int) -> List[int]:
-        # create a mapping of parent to children
-        parent_to_children = defaultdict(list)
-        for i in range(len(pid)):
-            parent_to_children[ppid[i]].append(pid[i])
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        # Helper function to check if Koko can eat all bananas within the given hours
+        def can_eat(piles: List[int], speed: int, hours: int) -> bool:
+            # Calculate the total hours required to eat all bananas at the current speed
+            total_hours = sum((pile - 1) // speed + 1 for pile in piles)
+            return total_hours <= hours
 
-        # BFS using deque
-        killed_processes = []
-        q = deque()
-        q.append(kill)
-        while q:
-            current = q.popleft()
-            killed_processes.append(current)
-            for child in parent_to_children[current]:
-                q.append(child)
+        # Binary search to find the minimum eating speed
+        left, right = 1, max(piles)
+        while left < right:
+            mid = (left + right) // 2
+            if can_eat(piles, mid, h):
+                right = mid
+            else:
+                left = mid + 1
 
-        return killed_processes
+        return left
 
-
-# Checking in PyCharm/terminal
 if __name__ == '__main__':
-    Instant = Solution()
-    Solve = Instant.killProcess(pid = [1,3,10,5], ppid = [3,0,5,3], kill = 5)
-    # pid = [1,3,10,5], ppid = [3,0,5,3], kill = 5 -> [5,10]
-    # pid = [1], ppid = [0], kill = 1 -> [1]
-    print(Solve)
+    solution = Solution()
+
+    # Test cases
+    assert solution.minEatingSpeed([3, 6, 7, 11], 8) == 4
+    assert solution.minEatingSpeed([30, 11, 23, 4, 20], 5) == 30
+    assert solution.minEatingSpeed([30, 11, 23, 4, 20], 6) == 23
+
+    print("All tests passed!")
