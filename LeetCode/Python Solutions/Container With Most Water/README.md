2fc446e4e3994f5228706de5401b7883bfeb4d90
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-04-23 23:29:44 +0300

    Added solution & description

--- LeetCode/Python Solutions/Container With Most Water/README.md
+++ LeetCode/Python Solutions/Container With Most Water/README.md
@@ -1,1 +1,1 @@
-Problem description of "Container With Most Water" can be found [here](https://leetcode.com/problems/container-with-most-water/solution/).
+Problem description of "Container With Most Water" can be found [here](https://leetcode.com/problems/container-with-most-water/solution/) and its solution [here](https://github.com/aurimas13/Solutions-To-Problems/blob/main/LeetCode/Python%20Solutions/Container%20With%20Most%20Water/container.py).
--- LeetCode/Python Solutions/Container With Most Water/container.py
+++ LeetCode/Python Solutions/Container With Most Water/container.py
@@ -1,49 +1,34 @@
 from typing import List
+
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        l,r,_maxs = 0,len(height) - 1,0
+        l, r, max_area = 0, len(height) - 1, 0
+
+        # Continue until pointers meet
         while l < r:
-            _maxs = max((r - l) * min(height[l],height[r]),_maxs)
+            # Calculate the area between the two heights and update the max area
+            max_area = max((r - l) * min(height[l], height[r]), max_area)
+
+            # Move the pointer with the smaller height inwards
             if height[l] < height[r]:
                 l += 1
             else:
                 r -= 1
-        return _maxs
 
+        return max_area
 
-# Checking in terminal/console:
 if __name__ == '__main__':
-    Sol = Solution()
-    Solve = Sol.maxArea([1,8,6,2,5,4,8,3,7])  # height = [1,8,6,2,5,4,8,3,7] -> 49
-    print(Solve)
+    s = Solution()
 
+    # Test cases
+    test_cases = [
+        ({"height": [1, 8, 6, 2, 5, 4, 8, 3, 7]}, 49),
+        ({"height": [1, 1]}, 1),
+        ({"height": [4, 3, 2, 1, 4]}, 16),
+        ({"height": [1, 2, 1]}, 2)
+    ]
 
-# OR
-#
-# class Solution:
-#     def maxArea(self, height: List[int]) -> int:
-#         maxarea = 0
-#         for left in range(len(height)):
-#             for right in range(left + 1, len(height)):
-#                 width = right - left
-#                 maxarea = max(maxarea, min(height[left], height[right]) * width)
-#
-#         return maxarea
-#
-# OR
-#
-# class Solution:
-#     def maxArea(self, height: List[int]) -> int:
-#         maxarea = 0
-#         left = 0
-#         right = len(height) - 1
-#
-#         while left < right:
-#             width = right - left
-#             maxarea = max(maxarea, min(height[left], height[right]) * width)
-#             if height[left] <= height[right]:
-#                 left += 1
-#             else:
-#                 right -= 1
-#
-#         return maxarea
+    for i, (test_input, expected_output) in enumerate(test_cases):
+        result = s.maxArea(**test_input)
+        assert result == expected_output, f"Test case {i} failed: expected {expected_output}, got {result}"
+        print(f"Test case {i} succeeded")
