111ee0ad7d0dad7a18389d4b7b94b2b3d6bc5999
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-04-23 12:22:16 +0300

    Added solution & description

--- LeetCode/Python Solutions/Find Minimum in Rotated Sorted Array/find.py
+++ LeetCode/Python Solutions/Find Minimum in Rotated Sorted Array/find.py
@@ -16,3 +16,4 @@ if __name__ == '__main__':
     Sol = Solution()
     Solve = Sol.findMin([11,13,15,17])  # [11,13,15,17] -> 11| nums = [3,4,5,1,2] -> 1 | nums = [4,5,6,7,0,1,2] -> 0
     print(Solve)
+
