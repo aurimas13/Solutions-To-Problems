4ce0ee157ff697322d94b450482405b946f2d80e
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-04-25 07:57:57 +0300

    Added solution & description

--- LeetCode/Python Solutions/Find the Index of the First Occurrence in a String /find.py
+++ LeetCode/Python Solutions/Find the Index of the First Occurrence in a String /find.py
@@ -1,39 +1,39 @@
-import heapq
-from typing import List
-
-
 class Solution:
-    def kthSmallest(self, mat: List[List[int]], k: int) -> int:
-        # Initialize the heap with the first element of each row
-        heap = [(sum(row[0] for row in mat), [0] * len(mat))]
-        # Initialize a set to store the visited states
-        visited = set(tuple(state) for _, state in heap)
-        # Loop until we find the kth smallest sum or the heap is empty
-        for i in range(k-1):
-            # Get the current smallest sum and state from the heap
-            current_sum, state = heapq.heappop(heap)
-            # Generate all possible next states by incrementing the index of the current element in each row
-            for j, index in enumerate(state):
-                if index < len(mat[j])-1:
-                    next_state = list(state)
-                    next_state[j] += 1
-                    # Check if we've already visited this state
-                    if tuple(next_state) not in visited:
-                        # Add the new sum and state to the heap and mark the state as visited
-                        next_sum = current_sum + mat[j][index+1] - mat[j][index]
-                        heapq.heappush(heap, (next_sum, next_state))
-                        visited.add(tuple(next_state))
-        # Return the kth smallest sum
-        return heap[0][0]
+    def strStr(self, haystack: str, needle: str) -> int:
+        # If needle is empty, return 0
+        if not needle:
+            return 0
 
+        # If haystack is empty, return -1
+        if not haystack:
+            return -1
 
-# Checking in terminal/console:
+        # Iterate through the haystack
+        for i in range(len(haystack) - len(needle) + 1):
+            # Check if the substring of haystack starting at index i matches the needle
+            if haystack[i:i + len(needle)] == needle:
+                return i
+
+        # If the needle is not found in the haystack, return -1
+        return -1
+
+# Test cases
 if __name__ == '__main__':
-    sol = Solution()
-    mat = [[1,3,11],[2,4,6]]
-    k = 5
-    print(sol.kthSmallest(mat, k))  # Expected output: 7
+    solution = Solution()
 
-    mat = [[1,3,11],[2,4,6]]
-    k = 9
-    print(sol.kthSmallest(mat, k))  # Expected output: 17
+    # Test case 1
+    haystack1 = "hello"
+    needle1 = "ll"
+    assert solution.strStr(haystack1, needle1) == 2
+
+    # Test case 2
+    haystack2 = "aaaaa"
+    needle2 = "bba"
+    assert solution.strStr(haystack2, needle2) == -1
+
+    # Test case 3
+    haystack3 = "abc"
+    needle3 = ""
+    assert solution.strStr(haystack3, needle3) == 0
+
+    print("All test cases passed!")
