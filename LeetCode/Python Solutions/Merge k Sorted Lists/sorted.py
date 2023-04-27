972cac4fff455c07a02c4a3e24107c1bc08dbb4c
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-04-27 10:08:49 +0300

    Added solution & description

--- LeetCode/Python Solutions/Merge k Sorted Lists/sorted.py
+++ LeetCode/Python Solutions/Merge k Sorted Lists/sorted.py
@@ -1,36 +1,69 @@
-class Solution(object):
-    def mergeKLists(self, lists):
-        """
-        :type lists: List[ListNode]
-        :rtype: ListNode
-        """
-        amount = len(lists)
-        interval = 1
-        while interval < amount:
-            for i in range(0, amount - interval, interval * 2):
-                lists[i] = self.merge2Lists(lists[i], lists[i + interval])
-            interval *= 2
-        return lists[0] if amount > 0 else None
+import heapq
+from typing import List, Optional
 
-    def merge2Lists(self, l1, l2):
-        head = point = ListNode(0)
-        while l1 and l2:
-            if l1.val <= l2.val:
-                point.next = l1
-                l1 = l1.next
-            else:
-                point.next = l2
-                l2 = l1
-                l1 = point.next.next
-            point = point.next
-        if not l1:
-            point.next=l2
-        else:
-            point.next=l1
-        return head.next
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-# Checking in PyCharm
+
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        # Create a dummy head node for the merged list
+        dummy_head = ListNode()
+        current = dummy_head
+
+        # Create a min-heap to store the smallest elements from each list
+        min_heap = [(node.val, index) for index, node in enumerate(lists) if node]
+        heapq.heapify(min_heap)
+
+        # Continue merging elements until the min-heap is empty
+        while min_heap:
+            # Pop the smallest element and its index from the min-heap
+            value, index = heapq.heappop(min_heap)
+
+            # Add the smallest element to the merged list
+            current.next = ListNode(value)
+            current = current.next
+
+            # Move the pointer in the original list and push the next element to the min-heap
+            lists[index] = lists[index].next
+            if lists[index]:
+                heapq.heappush(min_heap, (lists[index].val, index))
+
+        return dummy_head.next
+
+
 if __name__ == '__main__':
-    Instant = Solution()
-    Solve = Instant.mergeKLists([[1,4,5],[1,3,4],[2,6]])  # [[1,4,5],[1,3,4],[2,6]] -> [1,1,2,3,4,4,5,6]
-    print(Solve)
+    def list_to_linked_list(lst):
+        dummy_head = ListNode()
+        current = dummy_head
+        for val in lst:
+            current.next = ListNode(val)
+            current = current.next
+        return dummy_head.next
+
+    def linked_list_to_list(head):
+        lst = []
+        current = head
+        while current:
+            lst.append(current.val)
+            current = current.next
+        return lst
+
+    s = Solution()
+
+    test_cases = [
+        ([], []),
+        ([
+            [1, 4, 5],
+            [1, 3, 4],
+            [2, 6]
+        ], [1, 1, 2, 3, 4, 4, 5, 6]),
+    ]
+
+    for test_input, expected_output in test_cases:
+        input_lists = [list_to_linked_list(lst) for lst in test_input]
+        merged_head = s.mergeKLists(input_lists)
+        assert linked_list_to_list(merged_head) == expected_output
