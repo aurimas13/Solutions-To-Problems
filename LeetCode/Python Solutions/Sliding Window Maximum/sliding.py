19a757e6d3139b393cf168d34754a276a7940426
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-05-04 04:54:17 +0300

    Added solution & description

--- LeetCode/Python Solutions/Sliding Window Maximum/sliding.py
+++ LeetCode/Python Solutions/Sliding Window Maximum/sliding.py
@@ -1,55 +1,48 @@
 from collections import deque
 from typing import List
 
-
 class Solution:
     @staticmethod
     def maxSlidingWindow(nums: List[int], k: int) -> List[int]:
+        # Handle edge cases
         if not nums or len(nums) < k:
             raise ValueError()
 
+        # Initialize deque (double-ended queue) and result list
         window = deque()
         res = []
+
+        # Iterate through the input list
         for i in range(len(nums)):
+            # Remove elements that are out of the current sliding window from the front of the deque
             while window and (i - k) >= window[0][1]:
                 window.popleft()
 
+            # Remove elements smaller than the current element from the back of the deque
             while window and (nums[i] >= window[-1][0]):
                 window.pop()
 
+            # Append the current element and its index to the deque
             window.append((nums[i], i))
 
+            # Append the maximum element of the current sliding window to the result list
             if window and i >= k - 1:
                 res.append(window[0][0])
 
         return res
 
-
 # Checking in console
 if __name__ == '__main__':
-    Instant = Solution()
-    Solve = Instant.maxSlidingWindow(nums = [1,3,-1,-3,5,3,6,7], k = 3 )
-    # nums = [1,3,-1,-3,5,3,6,7], k = 3 -> [3,3,5,5,6,7]
-    # nums = [1], k = 1 -> [1]
-    print(Solve)
+    # Call the 'maxSlidingWindow' method with the input list and window size
+    result1 = Solution.maxSlidingWindow(nums=[1, 3, -1, -3, 5, 3, 6, 7], k=3)  # Expected output: [3, 3, 5, 5, 6, 7]
+    result2 = Solution.maxSlidingWindow(nums=[1], k=1)  # Expected output: [1]
+    result3 = Solution.maxSlidingWindow(nums=[1, -1], k=1)  # Expected output: [1, -1]
+    result4 = Solution.maxSlidingWindow(nums=[9, 11], k=2)  # Expected output: [11]
+    result5 = Solution.maxSlidingWindow(nums=[4, -2, -8, 5, -2, 7, 7, 2, -6, 5], k=4)  # Expected output: [5, 5, 7, 7, 7]
 
-
-# # Alternative method:
-# class Solution:
-#     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-#         if not nums or len(nums) < k:
-#             raise ValueError()
-#
-#         n = len(nums)
-#         left, right = [0] * (n + 1), [0] * (n + 1)
-#         left[-1], right[-1] = float('-inf'), float('-inf')
-#
-#         for i,j in zip(range(0, n), reversed(range(0, n))):
-#             left[i] = nums[i] if i % k == 0 else max(left[i-1], nums[i])
-#             right[j] = nums[j] if (j + 1) % k == 0 else max(right[j+1], nums[j])
-#
-#         res = []
-#         for i in range(n - k + 1):
-#             res.append(max(left[i + k - 1], right[i]))
-#
-#         return res
+    # Print the results
+    print(result1)
+    print(result2)
+    print(result3)
+    print(result4)
+    print(result5)
