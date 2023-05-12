418b7d16b61cda3460a8f80bf44b306576435e35
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-05-12 14:34:42 +0300

    Added solution & description

--- LeetCode/Python Solutions/Checking Existence of Edge Length Limited Paths/check.py
+++ LeetCode/Python Solutions/Checking Existence of Edge Length Limited Paths/check.py
@@ -1,4 +1,3 @@
-# Import typing to use List and Tuple types
 from typing import List, Tuple
 
 class Solution:
@@ -9,11 +8,27 @@ class Solution:
         # Initialize a list to store the parents of each node
         parents = list(range(n))
 
+        # Initialize a list to store the rank of each node
+        rank = [0] * n
+
         # Define a helper function to find the parent of a node
         def find(node: int) -> int:
             if parents[node] != node:
                 parents[node] = find(parents[node])
             return parents[node]
+
+        # Define a helper function to perform union of two nodes
+        def union(a: int, b: int) -> None:
+            root_a = find(a)
+            root_b = find(b)
+            if root_a != root_b:
+                if rank[root_a] < rank[root_b]:
+                    parents[root_a] = root_b
+                elif rank[root_a] > rank[root_b]:
+                    parents[root_b] = root_a
+                else:
+                    parents[root_b] = root_a
+                    rank[root_a] += 1
 
         # Initialize the result list for queries
         result = [False] * len(queries)
@@ -31,39 +46,10 @@ class Solution:
                 a, b, _ = edgeList[edge_idx]
                 edge_idx += 1
                 # Union the two sets containing nodes a and b
-                parents[find(a)] = find(b)
+                union(a, b)
 
-            # Check if the nodes p and q belong to the same set, and update the result list
-            result[query_idx] = find(p)
             # Check if the nodes p and q belong to the same set, and update the result list
             result[query_idx] = find(p) == find(q)
 
         # Return the result list containing the answers for all queries
         return result
-
-
-# Test the solution in the terminal
-if __name__ == "__main__":
-    # Instantiate the Solution class
-    solution = Solution()
-
-    # Test case 1
-    n1 = 3
-    edgeList1 = [[0, 1, 2], [1, 2, 4], [2, 0, 8], [1, 0, 16]]
-    queries1 = [[0, 1, 2], [0, 2, 5]]
-    assert solution.distanceLimitedPathsExist(n1, edgeList1, queries1) == [False, True]
-
-# Test case 2
-    n2 = 5
-    edgeList2 = [[0, 1, 10], [1, 2, 5], [2, 3, 9], [3, 4, 13]]
-    queries2 = [[0, 4, 14], [1, 3, 8], [0, 3, 7]]
-    assert solution.distanceLimitedPathsExist(n2, edgeList2, queries2) == [True, False, False]
-
-    # Test case 3
-    n3 = 6
-    edgeList3 = [[0, 1, 3], [1, 2, 4], [2, 3, 6], [3, 4, 7], [4, 5, 9], [0, 5, 12]]
-    queries3 = [[0, 3, 6], [0, 5, 10], [1, 4, 7], [2, 5, 10]]
-    assert solution.distanceLimitedPathsExist(n3, edgeList3, queries3) == [True, True, False, True]
-
-    print("All tests passed!")
-    
--- LeetCode/Python Solutions/Count Odd Numbers in an Interval Range/count.py
+++ LeetCode/Python Solutions/Count Odd Numbers in an Interval Range/count.py
@@ -6,7 +6,8 @@ class Solution:
 # Checking in Terminal/Console:
 if __name__ == '__main__':
     Sol = Solution()
-    Solve = Sol.countNodes(low = 3, high = 7)
+    Solve = Sol.countOdds(low = 3, high = 7)
     # low = 3, high = 7 -> 3
     # low = 8, high = 10 -> 1
     print(Solve)
+    
