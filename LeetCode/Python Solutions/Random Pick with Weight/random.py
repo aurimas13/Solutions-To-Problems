e51e3b0a6aa4a564c111f3d27c5bd2ad43e507e4
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-05-14 09:04:11 +0300

    Added solution & description

--- LeetCode/Python Solutions/Random Pick with Weight/random.py
+++ LeetCode/Python Solutions/Random Pick with Weight/random.py
@@ -1,16 +1,30 @@
+import random
 from typing import List
+from itertools import accumulate
 
 class Solution:
-    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
-        # sort the array of people in decreasing order of height and increasing order of k
-        people.sort(key=lambda x: (-x[0], x[1]))
-        
-        # initialize an empty list to store the reconstructed queue
-        queue = []
-        
-        # insert each person in the sorted array at the kth index in the queue
-        for p in people:
-            queue.insert(p[1], p)
-        
-        return queue
+    def __init__(self, w: List[int]):
+        # calculate prefix sums
+        self.prefix_sums = list(accumulate(w))
+        # total sum
+        self.total_sum = self.prefix_sums[-1]
 
+    def pickIndex(self) -> int:
+        # generate a random number
+        random_num = self.total_sum * random.random()
+        # binary search for the index
+        left, right = 0, len(self.prefix_sums)
+        while left < right:
+            mid = left + (right - left) // 2
+            if random_num > self.prefix_sums[mid]:
+                left = mid + 1
+            else:
+                right = mid
+        return left
+
+if __name__ == "__main__":
+    # test case
+    obj = Solution([1, 3, 4])
+    result = obj.pickIndex()
+    print(result)
+    # since the weights are 1, 3, 4, the result should be more likely to be 2
