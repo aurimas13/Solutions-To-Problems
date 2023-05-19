7ae46e2d433b1fe1e7eff65a48972fc5836969e5
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-05-19 10:16:08 +0300

    Added solution & description

--- LeetCode/Java Solutions/Is Graph Bipartite?/graph.java
+++ LeetCode/Java Solutions/Is Graph Bipartite?/graph.java
@@ -5,23 +5,31 @@ import java.util.Queue;
 public class Solution {
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
+        // Initialize color array for storing colors
         int[] colors = new int[n];
-        Arrays.fill(colors, -1);  // fill colors array with -1 indicating no color assigned yet
+        // fill colors array with -1 indicating no color assigned yet
+        Arrays.fill(colors, -1);  
 
-        for (int i = 0; i < n; ++i) {  // handle the case of non-connected graph
+        // handle the case of non-connected graph
+        for (int i = 0; i < n; ++i) {  
             if (colors[i] == -1) {
                 Queue<Integer> queue = new LinkedList<>();
+                // assign the first color
+                colors[i] = 0;  
                 queue.offer(i);
-                colors[i] = 0;  // assign the first color
                 
                 while (!queue.isEmpty()) {
                     int node = queue.poll();
                     for (int neighbour : graph[node]) {
-                        if (colors[neighbour] == -1) {  // if neighbour has not been colored yet
+                        // if neighbour has not been colored yet
+                        if (colors[neighbour] == -1) {  
                             queue.offer(neighbour);
-                            colors[neighbour] = colors[node] ^ 1;  // color it with the opposite color
-                        } else if (colors[neighbour] == colors[node]) {  // if neighbour has been colored and its color is the same as current node
-                            return false;  // the graph is not bipartite
+                            // color it with the opposite color
+                            colors[neighbour] = colors[node] ^ 1;  
+                        // if neighbour has been colored and its color is the same as current node
+                        } else if (colors[neighbour] == colors[node]) {  
+                            // the graph is not bipartite
+                            return false;  
                         }
                     }
                 }
