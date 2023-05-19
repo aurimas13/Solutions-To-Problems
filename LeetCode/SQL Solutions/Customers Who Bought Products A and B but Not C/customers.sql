e4481a16bd26a8be7fd909a1cb903655900ec62c
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-05-19 19:55:51 +0300

    Added solution & description

--- LeetCode/SQL Solutions/Customers Who Bought Products A and B but Not C/customers.sql
+++ LeetCode/SQL Solutions/Customers Who Bought Products A and B but Not C/customers.sql
@@ -1,4 +1,16 @@
-SELECT Customers.name AS Customers
-FROM Customers
-LEFT JOIN Orders ON Customers.id = Orders.customerId
-WHERE Orders.id IS NULL;
+SELECT c.customer_id, c.customer_name
+FROM Customers c
+JOIN (
+  SELECT o.customer_id
+  FROM Orders o
+  WHERE o.product_name IN ('A', 'B')
+  GROUP BY o.customer_id
+  HAVING COUNT(DISTINCT o.product_name) = 2
+) AS sub
+ON c.customer_id = sub.customer_id
+WHERE c.customer_id NOT IN (
+  SELECT o.customer_id
+  FROM Orders o
+  WHERE o.product_name = 'C'
+)
+ORDER BY c.customer_id;
