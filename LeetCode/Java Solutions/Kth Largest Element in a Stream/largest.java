051a27a14e299f89461bb520bdb27fafb7438b7e
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-05-23 09:08:15 +0300

    Added solution & description

--- LeetCode/Java Solutions/Kth Largest Element in a Stream/largest.java
+++ LeetCode/Java Solutions/Kth Largest Element in a Stream/largest.java
@@ -1,31 +1,33 @@
-import heapq
-from typing import List
+import java.util.PriorityQueue;
 
+class KthLargest {
+    private PriorityQueue<Integer> q;
+    private int k;
 
-class KthLargest:
-    def __init__(self, k: int, nums: List[int]):
-        self.k = k
-        self.heap = []
-        for num in nums:
-            self.add(num)
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        q = new PriorityQueue<Integer>(k);
+        for (int num : nums) {
+            add(num);
+        }
+    }
 
-    def add(self, val: int) -> int:
-        # If heap size is less than k, we can directly push the element
-        if len(self.heap) < self.k:
-            heapq.heappush(self.heap, val)
-        # Else we compare the incoming element with the minimum element (root of the heap)
-        # if incoming element is greater, it will replace the root
-        elif val > self.heap[0]:
-            heapq.heapreplace(self.heap, val)
-        # Return the root of the heap as the kth largest element
-        return self.heap[0]
+    public int add(int val) {
+        if (q.size() < k) {
+            q.offer(val);
+        } else if (q.peek() < val) {
+            q.poll();
+            q.offer(val);
+        }
+        return q.peek();
+    }
 
-
-# Tests:
-if __name__ == "__main__":
-    obj = KthLargest(3, [4, 5, 8, 2])
-    print(obj.add(3))  # returns 4
-    print(obj.add(5))  # returns 5
-    print(obj.add(10)) # returns 5
-    print(obj.add(9))  # returns 8
-    print(obj.add(4))  # returns 8
+    public static void main(String[] args) {
+        KthLargest kthLargest = new KthLargest(3, new int[] {4, 5, 8, 2});
+        System.out.println(kthLargest.add(3));  // returns 4
+        System.out.println(kthLargest.add(5));  // returns 5
+        System.out.println(kthLargest.add(10));  // returns 5
+        System.out.println(kthLargest.add(9));  // returns 8
+        System.out.println(kthLargest.add(4));  // returns 8
+    }
+}
