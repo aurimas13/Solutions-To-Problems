df2a8a1e64854d6845a4765c55ab2b8e5cfcd596
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-05-29 07:48:05 +0300

    Added solution & description

--- LeetCode/Python Solutions/Design Parking System/design.py
+++ LeetCode/Python Solutions/Design Parking System/design.py
@@ -9,14 +9,15 @@ class ParkingSystem:
         return False
 
 
-# Create a parking system with 3 big, 3 medium, and 3 small slots
-parkingSystem = ParkingSystem(3, 3, 3)
+if __name__ == "__main__":
+    # Create a parking system with 3 big, 3 medium, and 3 small slots
+    parkingSystem = ParkingSystem(3, 3, 3)
 
-# Add cars of different types
-car1Added = parkingSystem.addCar(1)  # Big car
-car2Added = parkingSystem.addCar(2)  # Medium car
-car3Added = parkingSystem.addCar(3)  # Small car
+    # Add cars of different types
+    car1Added = parkingSystem.addCar(1)  # Big car
+    car2Added = parkingSystem.addCar(2)  # Medium car
+    car3Added = parkingSystem.addCar(3)  # Small car
 
-print(f"Car 1 added: {car1Added}")
-print(f"Car 2 added: {car2Added}")
-print(f"Car 3 added: {car3Added}")
+    print(f"Car 1 added: {car1Added}")
+    print(f"Car 2 added: {car2Added}")
+    print(f"Car 3 added: {car3Added}")
