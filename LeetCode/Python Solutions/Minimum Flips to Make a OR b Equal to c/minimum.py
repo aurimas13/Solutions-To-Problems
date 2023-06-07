901035fc96b3289b4c49c062153476cfc8bccfe7
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-06-07 07:51:48 +0300

    Added solution & description

--- LeetCode/Python Solutions/Minimum Flips to Make a OR b Equal to c/minimum.py
+++ LeetCode/Python Solutions/Minimum Flips to Make a OR b Equal to c/minimum.py
@@ -1,25 +1,20 @@
-class Solution {
-    public int minFlips(int a, int b, int c) {
-        int flips = 0;
+class Solution:
+    def minFlips(self, a: int, b: int, c: int) -> int:
+        flips = 0
         
-        while(a > 0 || b > 0 || c > 0) {
-            int bitA = a & 1;
-            int bitB = b & 1;
-            int bitC = c & 1;
+        while a > 0 or b > 0 or c > 0:
+            bitA = a & 1
+            bitB = b & 1
+            bitC = c & 1
             
-            if(bitC == 0) {
-                if(bitA == 1 && bitB == 1) flips += 2;
-                else if(bitA == 1 || bitB == 1) flips++;
-            } else {
-                if(bitA == 0 && bitB == 0) flips++;
-            }
+            if bitC == 0:
+                if bitA == 1 and bitB == 1: flips += 2
+                elif bitA == 1 or bitB == 1: flips += 1
+            else:
+                if bitA == 0 and bitB == 0: flips += 1
+                
+            a >>= 1
+            b >>= 1
+            c >>= 1
             
-            a >>= 1;
-            b >>= 1;
-            c >>= 1;
-        }
-        
-        return flips;
-        
-    }
-}
+        return flips
