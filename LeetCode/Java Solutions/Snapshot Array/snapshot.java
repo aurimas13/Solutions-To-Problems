a531acfb65efcb13bbc30a220ad2621a4fab8722
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-06-11 08:40:30 +0300

    Added solution & description

--- LeetCode/Java Solutions/Snapshot Array/snapshot.java
+++ LeetCode/Java Solutions/Snapshot Array/snapshot.java
@@ -42,10 +42,4 @@ class SnapshotArray {
     }
 }
 
-/**
- * Your SnapshotArray object will be instantiated and called as such:
- * SnapshotArray obj = new SnapshotArray(length);
- * obj.set(index,val);
- * int param_2 = obj.snap();
- * int param_3 = obj.get(index,snap_id);
- */
+
