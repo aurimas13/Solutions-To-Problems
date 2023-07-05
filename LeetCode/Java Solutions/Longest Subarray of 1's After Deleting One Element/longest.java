77a5d4ca0f665f45065d1549e38f675429b7a87e
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-07-05 05:35:01 +0300

    Added solution & description

--- LeetCode/Java Solutions/Longest Subarray of 1's After Deleting One Element/longest.java
+++ LeetCode/Java Solutions/Longest Subarray of 1's After Deleting One Element/longest.java
@@ -1,31 +1,43 @@
-import java.util.*;
-
 class Solution {
-    /**
-     * Computes the length of the longest arithmetic subsequence in an array. 
-     * An arithmetic subsequence is a sequence of numbers in which the difference 
-     * between any two consecutive elements is the same. The function takes an 
-     * integer array `nums` as input and returns an integer representing the length 
-     * of the longest arithmetic subsequence.
-     *
-     * @param  nums  an integer array representing the input sequence
-     * @return       an integer representing the length of the longest arithmetic subsequence
-     */
-    public int longestArithSeqLength(int[] nums) {
-        int n = nums.length;
-        Map<Integer, Integer>[] dp = new HashMap[n];
-        int max_length = 2;
-
-        for (int i = 0; i < n; i++) {
-            dp[i] = new HashMap<>();
-            for (int j = 0; j < i; j++) {
-                int diff = nums[i] - nums[j];
-                dp[i].put(diff, dp[j].getOrDefault(diff, 1) + 1);
-                max_length = Math.max(max_length, dp[i].get(diff));
+    public int longestSubarray(int[] nums) {
+        // Keep track of the number of zeros seen
+        int zeros = 0;
+        
+        // Keep track of the current length of subarray of ones
+        int curr = 0;
+        
+        // Keep track of the maximum length of subarray of ones
+        int maxLen = 0;
+        
+        // Keep track of the length of subarray just before the last zero
+        int prev = 0;
+        
+        // Loop through each number in the array
+        for (int num : nums) {
+            // If the number is 1
+            if (num == 1) {
+                // Increment the current length
+                curr++;
+            // If the number is 0
+            } else {
+                // Increment the number of zeros seen
+                zeros++;
+                // Move the length of subarray just before the last zero
+                // to prev, and reset current length
+                prev = curr;
+                curr = 0;
             }
+            
+            // Update the maximum length
+            maxLen = Math.max(maxLen, prev + curr);
         }
-
-        return max_length;
+        
+        // If the array has at least one zero, then we can get the maximum
+        // length by deleting one element. Otherwise, we should reduce
+        // the length by 1 as we have to delete one element
+        return zeros > 0 ? maxLen : maxLen - 1;
     }
 }
 
+// Time complexity: O(n), where n is the number of elements in the input array.
+// Space complexity: O(1), as we are using only a constant amount of space.
