c70c9fc6c132ecbee2b5026b949f7285668e9d7f
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-07-19 08:03:06 +0300

    Added solution & description

--- LeetCode/Python Solutions/Non-overlapping Intervals/nonover.py
+++ LeetCode/Python Solutions/Non-overlapping Intervals/nonover.py
@@ -1,22 +1,25 @@
 from typing import List
+
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
-        # Sort by finish time
-        intervals.sort(key= lambda x: x[1])
-        dp = [1]* len(intervals)
-        prev = intervals[0]
-        for i in range(1, len(dp)):
-            if prev[1] > intervals[i][0]:
-                dp[i] = dp[i-1]
+        # If the list is empty, return 0
+        if not intervals:
+            return 0
+
+        # Sort the intervals by their end time
+        intervals.sort(key=lambda x: x[1])
+
+        # Initialize the end time to the first interval's end time
+        end = intervals[0][1]
+        count = 0
+
+        # Iterate through the intervals starting from the second one
+        for i in range(1, len(intervals)):
+            # If the current interval's start time is earlier than the last end time, increment the counter
+            if intervals[i][0] < end:
+                count += 1
             else:
-                dp[i] = dp[i-1] + 1
-                prev = intervals[i]
+                # Otherwise, update the end time
+                end = intervals[i][1]
 
-        return len(intervals) - dp[len(intervals)-1]
-
-
-# Checking in terminal/console:
-if __name__ == '__main__':
-    Sol = Solution()
-    Solve = Sol.eraseOverlapIntervals(intervals = [[1,2],[1,2],[1,2]])  # intervals = [[1,2],[1,2],[1,2]] -> 2
-    print(Solve)
+        return count
