fec2044ccc8753d22c3fd21a66ab127459440846
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-08-03 07:59:05 +0300

    Updated README.md

---
+++ LeetCode/Java Solutions/Letter Combinations of a Phone Number/combinations.java
@@ -0,0 +1,30 @@
+from typing import List
+from itertools import product
+
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        letters = [
+            None,
+            None,
+            "abc",
+            "def",
+            "ghi",
+            "jkl",
+            "mno",
+            "pqrs",
+            "tuv",
+            "wxyz"
+        ]
+        iterables = list(letters[int(n)] for n in digits)
+        return [''.join(x) for x in product(*iterables)]
+
+
+# Tests:
+if __name__ == '__main__':
+    Instant = Solution()
+    Solve = Instant.letterCombinations(digits="23")  
+    # digits = "23" -> ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+    print(Solve)
