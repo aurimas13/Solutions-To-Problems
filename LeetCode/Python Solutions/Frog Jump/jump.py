420ac647b3d42a4e5f3c024024159ed9e751ba46
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-08-27 05:34:05 +0300

    Added solution & description

--- LeetCode/Python Solutions/Frog Jump/jump.py
+++ LeetCode/Python Solutions/Frog Jump/jump.py
@@ -1,34 +1,40 @@
 from typing import List
 
-
 class Solution:
-    @staticmethod
-    def canCross(stones: List[int]) -> bool:
-        seen = set()
-        stoneSet = set(stones)
-        end = stones[-1]
-        stack = [(0, 0)]
-        while len(stack) > 0:
-            loc, steps = stack.pop()
-            if (loc, steps) in seen:
-                continue
-            seen.add((loc, steps))
-            if loc == end:
-                return True
-            elif loc < end:
-                for i in range(steps-1, steps+2):
-                    if i <= 0:
+    def canCross(self, stones: List[int]) -> bool:
+        if not stones:
+            return False
+        
+        n = len(stones)
+        if n == 1:
+            return True
+        if stones[1] != 1:
+            return False
+        
+        last_stone = stones[-1]
+        # Memoization
+        memo = {}
+        
+        for stone in stones:
+            memo[stone] = set()
+        
+        memo[0].add(0)
+        
+        for stone in stones:
+            for step in memo[stone]:
+                # Check all possible step sizes, i.e., k-1, k, k+1
+                for shift in [-1, 0, 1]:
+                    if step + shift <= 0:  # Ignore non-positive step sizes
                         continue
-                    if loc + i in stoneSet:
-                        stack.append((loc+i, i))
+                    next_jump = stone + step + shift
+                    if next_jump == last_stone:
+                        return True
+                    if next_jump in memo:
+                        memo[next_jump].add(step + shift)
+        
         return False
 
-
-# Checking in terminal/console:
-if __name__ == '__main__':
-    Sol = Solution()
-    Solve = Sol.canCross(stones=[0, 1, 3, 5, 6, 8, 12, 17])
-    # stones = [0,1,3,5,6,8,12,17] -> True
-    # stones = [0,1,2,3,4,8,9,11] -> False
-    print(Solve)
-
+# Example usage:
+solution = Solution()
+print(solution.canCross([0,1,3,5,6,8,12,17]))  # True
+print(solution.canCross([0,1,2,3,4,8,9,11]))  # False
