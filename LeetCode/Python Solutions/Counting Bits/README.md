7492d1e68bb90df714d59b915e925c5e611cfbaf
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-09-01 10:20:06 +0300

    Added solution & description

--- LeetCode/Python Solutions/Counting Bits/README.md
+++ LeetCode/Python Solutions/Counting Bits/README.md
@@ -1,1 +1,1 @@
-Problem description of "Counting Bits" can be found [here](https://leetcode.com/problems/counting-bits/).
+Problem description of "Counting Bits" can be found [here](https://leetcode.com/problems/counting-bits/description) and the solution [here](https://github.com/aurimas13/Solutions-To-Problems/blob/main/LeetCode/Python%20Solutions/Counting%20Bits/bits.py).
--- LeetCode/Python Solutions/Counting Bits/bits.py
+++ LeetCode/Python Solutions/Counting Bits/bits.py
@@ -1,18 +1,14 @@
 from typing import List
+
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        # Creating an empty list:
-        ones_list = []
-        # Looping over digits that make n:
-        for i in range(n+1):
-            cnt = list(bin(i)).count('1')
-            # cnt = list(map(str, bin(i))).count('1')
-            ones_list.append(cnt)
-        return ones_list
-
-
-# Checking in PyCharm:
-if __name__ == '__main__':
-    Sol = Solution()
-    Solve = Sol.countBits(2)  # for 2 - [0,1,1] | for 5 - [0,1,1,2,1,2]
-    print(Solve)
+        # Initialize the result array with size n+1 and first element as 0
+        res = [0] * (n + 1)
+        
+        # Loop from 1 to n to compute the number of 1's for each number
+        for i in range(1, n + 1):
+            # If i is even, number of 1's is same as number of 1's in i/2
+            # If i is odd, number of 1's is number of 1's in i-1 plus 1
+            res[i] = res[i >> 1] + (i & 1)
+        
+        return res
