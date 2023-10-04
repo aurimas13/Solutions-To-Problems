606c2328e05f4db440c3fd209431aa0b5f79bca2
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-10-04 19:35:17 +0300

    Added solution & description

--- LeetCode/Pandas Solutions/Sellers With No Sales/sellers.py
+++ LeetCode/Pandas Solutions/Sellers With No Sales/sellers.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+# Define the function
 def sellers_with_no_sales(customer: pd.DataFrame, orders: pd.DataFrame, seller: pd.DataFrame) -> pd.DataFrame:
     # Step 1: Filter the Orders table for 2020 sales
     sales_2020 = orders[orders['sale_date'].dt.year == 2020]
@@ -17,15 +18,16 @@ def sellers_with_no_sales(customer: pd.DataFrame, orders: pd.DataFrame, seller: 
     return result[['seller_name']]
 
 if __name__ == '__main__':
-    customer = pd.DataFrame({'customer_id': [1, 2, 3, 4, 5], 'customer_name': ['Daniel', 'Elizabeth', 'Jules', 'Adam', 'Amelia']})
-    orders = pd.DataFrame({'order_id': [1, 2, 3, 4, 5, 6, 7, 8], 'sale_date': ['2020-01-01', '2020-02-01', '2020-02-01', '2020-03-01', '2020-03-01', '2020-03-01', '2020-04-01', '2020-04-01'], 'customer_id': [1, 1, 2, 2, 2, 3, 4, 5], 'seller_id': [1, 2, 3, 1, 2, 3, 1, 1]})
-    seller = pd.DataFrame({'seller_id': [1, 2, 3, 4, 5], 'seller_name': ['Daniel', 'Elizabeth', 'Jules', 'Adam', 'Amelia']})
-    result = sellers_with_no_sales(customer, orders, seller)
-    print(result)
-    
-    #   seller_name
-    # 0        Adam
-    # 1      Amelia
-    # 2      Jules
+    # Sample Data
+    data = [[101, 'Alice'], [102, 'Bob'], [103, 'Charlie']]
+    customer = pd.DataFrame(data, columns=['customer_id', 'customer_name']).astype({'customer_id':'Int64', 'customer_name':'object'})
+    data = [[1, '2020-03-01', 1500, 101, 1], [2, '2020-05-25', 2400, 102, 2], [3, '2019-05-25', 800, 101, 3], [4, '2020-09-13', 1000, 103, 2], [5, '2019-02-11', 700, 101, 2]]
+    orders = pd.DataFrame(data, columns=['order_id', 'sale_date', 'order_cost', 'customer_id', 'seller_id']).astype({'order_id':'Int64', 'sale_date':'datetime64[ns]', 'order_cost':'Int64', 'customer_id':'Int64', 'seller_id':'Int64'})
+    data = [[1, 'Daniel'], [2, 'Elizabeth'], [3, 'Frank']]
+    seller = pd.DataFrame(data, columns=['seller_id', 'seller_name']).astype({'seller_id':'Int64', 'seller_name':'object'})
 
+    # Test the function
+    print(sellers_with_no_sales(customer, orders, seller))
     
+
+
