9ea18e94cff366431e009eee5ae888cbd7ce9de1
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-12-09 22:24:15 +0200

    Updated README.md

---
+++ LeetCode/Java Solutions/Binary Tree Inorder Traversal/binary.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+
+        while (current != null || !stack.isEmpty()) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+
+            current = stack.pop();
+            result.add(current.val);
+            current = current.right;
+        }
+
+        return result;
+    }
+}
---
+++ LeetCode/Python Solutions/Binary Tree Inorder Traversal/binary.py
@@ -0,0 +1,11 @@
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        def inorder(node, result):
+            if node:
+                inorder(node.left, result)
+                result.append(node.val)
+                inorder(node.right, result)
+        
+        result = []
+        inorder(root, result)
+        return result  
