9a1c729010d695028324cb53aeafba2bc745f160
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-12-18 08:16:31 +0200

    Added solution & description

---
+++ LeetCode/Python Solutions/Maximum Product Difference Between Two Pairs/maximum.py
@@ -0,0 +1,6 @@
+from typing import List
+
+class Solution:
+    def maxProductDifference(self, nums: List[int]) -> int:
+        nums.sort()
+        return (nums[-1] * nums[-2]) - (nums[0] * nums[1])
