64986acae53ed7c20fa2d5e02c7136f5dc7889ac
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2023-12-26 22:36:19 +0200

    Updated README.md

---
+++ LeetCode/Java Solutions/Number of Dice Rolls With Target Sum/number.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int numRollsToTarget(int n, int k, int target) {
+        int MOD = 1000000007;
+
+        // Initialize the dp array.
+        int[][] dp = new int[n + 1][target + 1];
+        dp[0][0] = 1;  // Base case: 1 way to achieve sum 0 with 0 dice
+
+        for (int dice = 1; dice <= n; dice++) {
+            for (int t = 1; t <= target; t++) {
+                for (int face = 1; face <= k && face <= t; face++) {
+                    dp[dice][t] = (dp[dice][t] + dp[dice - 1][t - face]) % MOD;
+                }
+            }
+        }
+
+        return dp[n][target];
+    }
+}
