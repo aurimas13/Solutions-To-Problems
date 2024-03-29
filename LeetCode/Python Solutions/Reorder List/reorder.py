471c55fea5817b7f0057f65ffdc1fca4ba2ea99c
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2024-03-29 08:43:56 +0200

    Added solution & description

--- LeetCode/Python Solutions/Reorder List/reorder.py
+++ LeetCode/Python Solutions/Reorder List/reorder.py
@@ -1,47 +1,38 @@
-from typing import Optional
-
 class ListNode:
-   def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
+    def __init__(self, val=0, next=None):
+<<<<<<<<<<<<<<  ✨ Codeium Command 🌟 >>>>>>>>>>>>>>>>
+-        self.val = val
+-        self.next = next
 
 class Solution:
-   def reorderList(self, head: Optional[ListNode]) -> None:
-      """
-      Do not return anything, modify head in-place instead.
-      """
-      curr = head
-      nodes = []
-      while curr:
-         nodes.append(curr)
-         curr = curr.next
-
-      l, r = 0, len(nodes) - 1
-      while l < r:
-         nodes[l].next, nodes[r].next = (
-            nodes[r],
-            nodes[l].next,
-         )
-
-         l += 1
-         r -= 1
-
-      nodes[l].next = None
-
-
-# Test:
-if __name__ == '__main__':
-    val = ListNode(1)
-    val.next = ListNode(2)
-    val.next.next = ListNode(3)
-    val.next.next.next = ListNode(4)
-    val.next.next.next.next = ListNode(5)
-    Instant = Solution()
-    Instant.reorderList(val)  # head = [1,2,3,4,5] -> [1,5,2,4,3]
-
-    # Print reordered list
-    curr = val
-    while curr:
-        print(curr.val, end=" ")
-        curr = curr.next
++    """
++    @param head: The head of the linked list
++    @return: nothing
++    """
+<<<<<<<  2794d946-bfee-4795-b6aa-864d746a96a3  >>>>>>>
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        if not head or not head.next:
+            return
+        
+        # Find the middle of the list
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+        # Reverse the second half
+        prev, curr = None, slow.next
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        slow.next = None  # Cut off the first half
+        
+        # Merge two halves
+        first, second = head, prev
+        while second:
+            temp1, temp2 = first.next, second.next
+            first.next = second
+            second.next = temp1
+            first, second = temp1, temp2
