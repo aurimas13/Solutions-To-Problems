723a58d35dbdf15ce1fe8e50f6ed084041b30fbb
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2024-04-29 05:44:18 +0300

    Added solution & description

---
+++ LeetCode/Java Solutions/Minimum Number of Operations to Make Array XOR Equal to K/minumum.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int minOperations(int[] nums, int k) {
+        int currentXor = 0;
+        
+        // Calculate the initial XOR of all numbers
+        for (int num : nums) {
+            currentXor ^= num;
+        }
+        
+        // Calculate the required XOR to make the XOR of the array equal to k
+        int requiredXor = currentXor ^ k;
+        
+        // Count the number of 1's in the binary representation of requiredXor
+        return Integer.bitCount(requiredXor);
+    }
+}
