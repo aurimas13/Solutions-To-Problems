0b5725a38bfe53cc11018802967b331cea6fe059
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2024-05-16 07:24:28 +0100

    Added solution & description

--- LeetCode/Pandas Solutions/Countries You Can Safely Invest In/countries.py
+++ LeetCode/Pandas Solutions/Countries You Can Safely Invest In/countries.py
@@ -14,6 +14,9 @@ def find_safe_countries(person: pd.DataFrame, country: pd.DataFrame, calls: pd.D
         calls_with_callee[['duration', 'callee_country_code']].rename(columns={'callee_country_code': 'country_code'})
     ])
     
+    # Remove calls where country_code is NaN
+    combined_calls = combined_calls.dropna(subset=['country_code'])
+    
     # Calculate global average call duration
     global_avg_duration = combined_calls['duration'].mean()
     
@@ -29,13 +32,13 @@ def find_safe_countries(person: pd.DataFrame, country: pd.DataFrame, calls: pd.D
     return result
 
 # Example usage
-data = [[3, 'Jonathan', '051-1234567'], [12, 'Elvis', '051-7654321'], [1, 'Moncef', '212-1234567'], [2, 'Maroua', '212-6523651'], [7, 'Meir', '972-1234567'], [9, 'Rachel', '972-0011100']]
+data = [[376, 'Gavriel', '078-6192794'], [369, 'Dalia', '637-0879945'], [130, 'Yaffah', '637-0126340'], [100, 'Naftali', '122-3190478'], [502, 'Gavriel', '870-5834859'], [426, 'Yehudit', '870-4713239']]
 person = pd.DataFrame(data, columns=['id', 'name', 'phone_number']).astype({'id': 'Int64', 'name': 'object', 'phone_number': 'object'})
 
-data = [['Peru', '051'], ['Israel', '972'], ['Morocco', '212'], ['Germany', '049'], ['Ethiopia', '251']]
+data = [['Brazil', '356'], ['Turkey', '379'], ['China', '078'], ['Israel', '120'], ['Algeria', '283'], ['Japan', '122']]
 country = pd.DataFrame(data, columns=['name', 'country_code']).astype({'name': 'object', 'country_code': 'object'})
 
-data = [[1, 9, 33], [2, 9, 4], [1, 2, 59], [3, 12, 102], [3, 12, 330], [12, 3, 5], [7, 9, 13], [7, 1, 3], [9, 7, 1], [1, 7, 7]]
+data = [[548, 13, 297], [801, 549, 265], [426, 368, 335], [948, 618, 543], [503, 426, 907], [217, 376, 631]]
 calls = pd.DataFrame(data, columns=['caller_id', 'callee_id', 'duration']).astype({'caller_id': 'Int64', 'callee_id': 'Int64', 'duration': 'Int64'})
 
 # Get the countries where the company can invest
