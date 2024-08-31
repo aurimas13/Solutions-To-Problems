39b24f31f5f7b3223fd7067fd022df69465e8f74
Aurimas Aleksandras <aurimasaleksandras@Aurimass-MacBook-Pro.local>
2024-08-31 05:36:30 +0300

    Update README.md

--- LeetCode/Java Solutions/Binary Tree Postorder Traversal/binary.java
+++ LeetCode/Java Solutions/Binary Tree Postorder Traversal/binary.java
@@ -1,13 +1,15 @@
-class Solution:
-    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        result = []
-        
-        def postorder(node):
-            if not node:
-                return
-            postorder(node.left)
-            postorder(node.right)
-            result.append(node.val)
-        
-        postorder(root)
-        return result
+class Solution {
+    private List<Integer> result = new ArrayList<>();
+    
+    public List<Integer> postorderTraversal(TreeNode root) {
+        postorder(root);
+        return result;
+    }
+    
+    private void postorder(TreeNode node) {
+        if (node == null) return;
+        postorder(node.left);
+        postorder(node.right);
+        result.add(node.val);
+    }
+}
--- LeetCode/Python Solutions/Fraction Addition and Subtraction/README.md
+++ LeetCode/Python Solutions/Fraction Addition and Subtraction/README.md
@@ -1,3 +1,1 @@
 The Problem description of "Fraction Addition and Subtraction" is found [here](https://leetcode.com/problems/fraction-addition-and-subtraction/) while the solution is found [here](https://github.com/aurimas13/Solutions-To-Problems/blob/main/LeetCode/Python%20Solutions/Fraction%20Addition%20and%20Subtraction/fraction.py).
-
-To check the solution in terminal first compile Java file as `javac fraction.java`, then run the command as follows `java Solution` and it will check tests and if the solution works correctly.
